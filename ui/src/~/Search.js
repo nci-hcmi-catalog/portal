180f2cc35f9a8352fdc58ed2444e8db2e3cf9ea6
Alex Wilmer <djazium@gmail.com>
2018-03-15 16:37:30 -0400

    use css not injectglobal

--- ui/src/~/Search.js
+++ ui/src/~/Search.js
@@ -1,63 +1,10 @@
 import React from 'react';
-import { css } from 'emotion';
 import { Link } from 'react-router-dom';
 import { Arranger, Aggregations, CurrentSQON, Table } from '@arranger/components/dist/Arranger';
 import '@arranger/components/public/themeStyles/beagle/beagle.css';
+import searchStyles from '~/searchStyles';
 import Url from '~/Url';
 import { Row } from '~/Layout';
-
-let style = css`
-  .ReactTable .rt-resizable-header-content {
-    color: #900;
-  }
-
-  .ReactTable.-striped .rt-tr.-odd {
-    background-color: #fff2cc;
-  }
-
-  .ReactTable .rt-thead {
-    background-color: #ffffff;
-  }
-
-  .sqon-view {
-    background-color: #ffffff;
-    border: 1px solid #d4d6dd;
-    padding: 4px 19px 14px;
-  }
-
-  .sqon-value {
-    background-color: #900;
-    color: #fff;
-    padding: 0 7px 0 12px;
-    margin-right: 4px;
-    cursor: pointer;
-  }
-
-  .aggregation-card .title-wrapper .title {
-    color: #900;
-    font-weight: bolder;
-    font-size: 0.9rem;
-    font-family: Open Sans, sans-serif;
-  }
-
-  .aggregation-card {
-    border-left-color: #774928 !important;
-  }
-
-  .aggregation-card .bucket-item .bucket-count {
-    border-radius: 5px;
-    background-color: #fff2cc;
-    color: #343434;
-    padding-left: 10px;
-    padding-right: 10px;
-    font-family: Open Sans, sans-serif;
-    display: -ms-flexbox;
-    display: flex;
-    -ms-flex-align: center;
-    align-items: center;
-    margin-left: auto;
-  }
-`;
 
 export default props => (
   <Url
@@ -68,7 +15,7 @@ export default props => (
           projectId={props.version}
           render={props => {
             return (
-              <div css={style}>
+              <div css={searchStyles}>
                 <Row>
                   <Aggregations
                     {...{
@@ -78,22 +25,16 @@ export default props => (
                       graphqlField: 'models',
                     }}
                   />
-                  <div
+                  <Col
+                    p={30}
                     style={{
                       position: 'relative',
                       flexGrow: 1,
-                      display: 'flex',
-                      flexDirection: 'column',
-                      padding: 30,
                     }}
                   >
-                    <div
-                      css={`
-                        display: flex;
-                      `}
-                    >
+                    <Row>
                       <CurrentSQON {...props} {...url} index="models" graphqlField="models" />
-                    </div>
+                    </Row>
                     <Table
                       {...props}
                       {...url}
@@ -105,7 +46,7 @@ export default props => (
                       columnDropdownText="Columns"
                       fieldTypesForFilter={['text', 'keyword', 'id']}
                     />
-                  </div>
+                  </Col>
                 </Row>
               </div>
             );
