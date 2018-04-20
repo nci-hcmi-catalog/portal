06c4054bea3499aa365bcad7ab5ec0d2c368465b
Alex Wilmer <djazium@gmail.com>
2018-04-19 22:02:06 -0400

    🎨 styled-system theme

--- ui/src/components/App.js
+++ ui/src/components/App.js
@@ -1,8 +1,9 @@
 import 'babel-polyfill';
 import React from 'react';
 import Component from 'react-component-component';
-import { Dashboard as ArrangerDashboard } from '@arranger/components';
+import { ThemeProvider } from 'emotion-theming';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { Dashboard as ArrangerDashboard } from '@arranger/components';
 import globals from 'utils/globals';
 import Search from 'components/Search';
 import Model from 'components/Model';
@@ -10,90 +11,93 @@ import AdminNav from 'components/AdminNav';
 import { Row, Col, Box } from 'components/Layout';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
+import theme from 'theme';
 
 export default () => (
-  <Router>
-    <Component
-      initialState={{
-        version: globals.VERSION,
-      }}
-    >
-      {({ state }) => (
-        <Switch>
-          <Route
-            path="/"
-            exact
-            render={() => (
-              <Col>
-                <Row>
-                  <Row
-                    p={15}
-                    css={`
-                      color: #900;
-                      font-weight: bold;
-                      font-size: 1.3em;
-                    `}
-                  >
-                    HCMI Data Portal
+  <ThemeProvider theme={theme}>
+    <Router>
+      <Component
+        initialState={{
+          version: globals.VERSION,
+        }}
+      >
+        {({ state }) => (
+          <Switch>
+            <Route
+              path="/"
+              exact
+              render={() => (
+                <Col>
+                  <Row>
+                    <Row
+                      p={15}
+                      css={`
+                        color: #900;
+                        font-weight: bold;
+                        font-size: 1.3em;
+                      `}
+                    >
+                      HCMI Data Portal
+                    </Row>
+                    <Row ml="auto">
+                      <Link to="/admin/manage_users">
+                        <Box p={15}>Administrator</Box>
+                      </Link>
+                    </Row>
                   </Row>
-                  <Row ml="auto">
-                    <Link to="/admin/manage_users">
-                      <Box p={15}>Administrator</Box>
-                    </Link>
+                  <Search version={state.version} index="models" />
+                </Col>
+              )}
+            />
+            <Route
+              path="/arranger"
+              render={({ match }) => <ArrangerDashboard basename={match.url} />}
+            />
+            <Route
+              path="/admin"
+              render={({ match }) => (
+                <Col>
+                  <Row>
+                    <Row p={15}>
+                      <Link to="/">« Back to List View</Link>
+                    </Row>
+                    <Row flex={1} p={15}>
+                      HCMI Searchable Catalog Administration
+                    </Row>
+                    <Row p={15}>Logout</Row>
                   </Row>
-                </Row>
-                <Search version={state.version} index="models" />
-              </Col>
-            )}
-          />
-          <Route
-            path="/arranger"
-            render={({ match }) => <ArrangerDashboard basename={match.url} />}
-          />
-          <Route
-            path="/admin"
-            render={({ match }) => (
-              <Col>
-                <Row>
-                  <Row p={15}>
-                    <Link to="/">« Back to List View</Link>
+                  <Row>
+                    <AdminNav />
+                    <Route path="/admin/manage_users" render={() => <Row p={15}>Users</Row>} />
+                    <Route
+                      path="/admin/single_model_upload"
+                      render={() => <Row p={15}>single model upload</Row>}
+                    />
+                    <Route
+                      path="/admin/bulk_model_upload"
+                      render={() => <Row p={15}>bulk model upload</Row>}
+                    />
+                    <Route
+                      path="/admin/manage_models"
+                      render={() => <Row p={15}>manage models</Row>}
+                    />
                   </Row>
-                  <Row flex={1} p={15}>
-                    HCMI Searchable Catalog Administration
-                  </Row>
-                  <Row p={15}>Logout</Row>
-                </Row>
-                <Row>
-                  <AdminNav />
-                  <Route path="/admin/manage_users" render={() => <Row p={15}>Users</Row>} />
-                  <Route
-                    path="/admin/single_model_upload"
-                    render={() => <Row p={15}>single model upload</Row>}
-                  />
-                  <Route
-                    path="/admin/bulk_model_upload"
-                    render={() => <Row p={15}>bulk model upload</Row>}
-                  />
-                  <Route
-                    path="/admin/manage_models"
-                    render={() => <Row p={15}>manage models</Row>}
-                  />
-                </Row>
-              </Col>
-            )}
-          />
-          <Route
-            path="/model/:modelName"
-            render={({ match }) => (
-              <div>
-                <Header />
-                <Model modelName={match.params.modelName} />
-                <Footer />
-              </div>
-            )}
-          />
-        </Switch>
-      )}
-    </Component>
-  </Router>
+                </Col>
+              )}
+            />
+            <Route
+              path="/model/:modelName"
+              render={({ match }) => (
+                <div>
+                  <Header />
+                  <Model modelName={match.params.modelName} />
+                  <Footer />
+                </div>
+              )}
+            />
+          </Switch>
+        )}
+      </Component>
+    </Router>
+  </ThemeProvider>
 );
---
+++ ui/src/theme/index.js
@@ -0,0 +1,8 @@
+export default {
+  buttons: {
+    primary: {
+      backgroundColor: 'rgb(144,0,0,0.7)',
+      color: 'white',
+    },
+  },
+};
