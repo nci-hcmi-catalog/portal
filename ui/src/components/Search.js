90c20e12a6aed78a3e6c955ccaaa9422dd3f6a1b
Alex Wilmer <djazium@gmail.com>
2018-04-22 20:38:11 -0400

    ✨ placeholder charts

--- ui/src/components/Search.js
+++ ui/src/components/Search.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Component from 'react-component-component';
 import { Arranger, Aggregations, CurrentSQON, Table } from '@arranger/components/dist/Arranger';
 import '@arranger/components/public/themeStyles/beagle/beagle.css';
+import { VictoryChart, VictoryBar, VictoryPie } from 'victory';
 
 import searchStyles from 'theme/searchStyles';
 import Url from 'components/Url';
@@ -35,6 +36,90 @@ export default props => (
                         />
                       </Col>
                       <Col className="search-results-wrapper" p={30} flex={1}>
+                        <Row
+                          bg="white"
+                          css={`
+                            height: 170px;
+                            padding: 16px;
+                          `}
+                        >
+                          <Col alignItems="center">
+                            <span
+                              className="sqon-field"
+                              css={`
+                                font-size: 12px;
+                              `}
+                            >
+                              Models by Primary Site
+                            </span>
+                            <VictoryPie
+                              style={{
+                                labels: {
+                                  display: 'none',
+                                },
+                              }}
+                              innerRadius={100}
+                              colorScale={['#ee7f6d', '#e96535', '#f5b464']}
+                              data={[
+                                { x: 'Cats', y: 35 },
+                                { x: 'Dogs', y: 40 },
+                                { x: 'Birds', y: 55 },
+                              ]}
+                            />
+                          </Col>
+                          <Col alignItems="center">
+                            {' '}
+                            <span
+                              className="sqon-field"
+                              css={`
+                                font-size: 12px;
+                              `}
+                            >
+                              Top Variants
+                            </span>
+                            <VictoryChart domainPadding={100}>
+                              <VictoryBar
+                                style={{
+                                  data: {
+                                    fill: d => {
+                                      return ['#ee7f6d', '#e96535', '#f5b464'][d.eventKey % 3];
+                                    },
+                                  },
+                                }}
+                                data={[
+                                  { x: 'Cats', y: 55 },
+                                  { x: 'Dogs', y: 40 },
+                                  { x: 'Birds', y: 35 },
+                                ]}
+                              />
+                            </VictoryChart>
+                          </Col>
+                          <Col alignItems="center">
+                            {' '}
+                            <span
+                              className="sqon-field"
+                              css={`
+                                font-size: 12px;
+                              `}
+                            >
+                              2D versus 3D Growth
+                            </span>
+                            <VictoryPie
+                              style={{
+                                labels: {
+                                  display: 'none',
+                                },
+                              }}
+                              innerRadius={100}
+                              colorScale={['#ee7f6d', '#e96535', '#f5b464']}
+                              data={[
+                                { x: 'Cats', y: 35 },
+                                { x: 'Dogs', y: 40 },
+                                { x: 'Birds', y: 55 },
+                              ]}
+                            />
+                          </Col>
+                        </Row>
                         <Row>
                           {!sqon && (
                             <Row
