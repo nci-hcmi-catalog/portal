b63505f5524fe8c572aeae2af122ff71a2781f8e
Alex Wilmer <djazium@gmail.com>
2018-05-02 16:00:12 -0400

    basic mongoose / mongo setup

--- api/src/index.js
+++ api/src/index.js
@@ -5,6 +5,12 @@ import { Server } from 'http';
 import { rainbow } from 'chalk-animation';
 import Arranger from '@arranger/server';
 import cors from 'cors';
+import mongoose from 'mongoose';
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/test');
+
+// create Cell model
+const Cell = mongoose.model('Cell', { name: String });
 
 const port = process.env.PORT || 5050;
 const app = express();
@@ -14,6 +20,15 @@ app.use(cors());
 
 Arranger({ io }).then(router => {
   app.use(router);
+
+  // save new cell placeholder endpoint
+  app.post('/saveCell', async (req, res) => {
+    const cell = new Cell({ name: req.body.cellName || 'Zildjian' });
+    await cell.save();
+    console.log('meow');
+    res.json({ message: 'cell saved succesfully' });
+  });
+
   http.listen(port, async () => {
     rainbow(`⚡️ Listening on port ${port} ⚡️`);
   });
