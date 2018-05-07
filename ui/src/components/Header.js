1f1a49c5a6767a4f6454c6b20d1150063f8b5ae5
Alex Wilmer <djazium@gmail.com>
2018-05-07 10:52:58 -0400

    💄 header: new logo, updated text, gradient

--- ui/src/components/Header.js
+++ ui/src/components/Header.js
@@ -3,44 +3,67 @@ import bannerPath from 'assets/hcmi-dna-banner.jpg';
 import logoPath from 'assets/logo-NIH-HCMI-Catalog.svg';
 import { Link } from 'react-router-dom';
 
-export default () => (
-  <Link
-    to="/"
+let Gradient = p => (
+  <div
     css={`
-      display: flex;
-      background: #fff0ce;
-      background-image: url(${bannerPath});
       height: 88px;
-      background-repeat: no-repeat;
-      background-position: right;
-      justify-content: space-between;
-      align-items: center;
-      padding: 0 23px;
-      text-decoration: none;
+      position: absolute;
+      width: 100%;
+      ${p.xcss};
     `}
-  >
-    <img
-      src={logoPath}
-      alt="HCMI Catalog logo"
-      css={`
-        height: 51px;
-      `}
+  />
+);
+
+export default () => (
+  <>
+    <Gradient
+      xcss={`
+       background-image: -webkit-gradient(
+         linear,
+         left top,
+         right top,
+         from(rgba(255, 255, 255, 0.7)),
+         to(rgba(0, 0, 0, 0))
+       );`}
     />
-    <div
+    <Link
+      to="/"
       css={`
-        text-transform: uppercase;
-        font-family: 'Libre Franklin';
-        font-size: 30px;
-        font-style: normal;
-        font-stretch: normal;
-        line-height: 0.87;
-        letter-spacing: 0.04em;
-        text-align: left;
-        color: #ffffff;
-        text-shadow: 0 0 4.9px rgba(50, 50, 50, 0.44);
+        display: flex;
+        background: #fff0ce;
+        background-image: url(${bannerPath});
+        height: 88px;
+        background-repeat: no-repeat;
+        background-position: right;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0 23px;
+        text-decoration: none;
       `}
     >
-      Human Cancer Models Initiative
-    </div>
-  </Link>
+      <img
+        src={logoPath}
+        alt="HCMI Catalog logo"
+        css={`
+          height: 51px;
+          z-index: 1;
+        `}
+      />
+      <div
+        css={`
+          font-family: 'Libre Franklin';
+          font-size: 26px;
+          font-weight: bold;
+          font-stretch: normal;
+          line-height: 0.87;
+          letter-spacing: 0.04em;
+          text-align: left;
+          color: #ffffff;
+          text-shadow: 0 0 4.9px rgba(50, 50, 50, 0.44);
+        `}
+      >
+        Models Catalog
+      </div>
+    </Link>
+  </>
 );
