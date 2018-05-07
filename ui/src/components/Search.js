1a58140a534272a0fb022dfa85335a973b276f1e
Alex Wilmer <djazium@gmail.com>
2018-05-07 17:03:21 -0400

    ✨ draggable agg panel (#51)
    ♻️ remove some nesting

--- ui/src/components/Search.js
+++ ui/src/components/Search.js
@@ -1,124 +1,139 @@
 import React from 'react';
 import Component from 'react-component-component';
-import { Arranger, Aggregations, CurrentSQON, Table } from '@arranger/components/dist/Arranger';
+import { Aggregations, CurrentSQON, Table } from '@arranger/components/dist/Arranger';
 import '@arranger/components/public/themeStyles/beagle/beagle.css';
+import SplitPane from 'react-split-pane';
 import searchStyles from 'theme/searchStyles';
-import Url from 'components/Url';
 import Quicksearch from 'components/Quicksearch';
 import PrimarySiteChart from 'components/PrimarySiteChart';
 import GrowthChart from 'components/GrowthChart';
 import TableEntity from 'components/TableEntity';
 import { Row, Col } from 'theme/system';
-import { SavedSetsContext } from 'providers/SavedSets';
 import { SelectedModelsContext } from 'providers/SelectedModels';
 
-export default props => (
-  <Component initalState={{ sorted: [] }}>
-    {({ setState, state }) => (
-      <Url
-        render={({ sqon, setSQON, history }) => (
-          <SavedSetsContext.Consumer>
-            {savedSetsContext => (
-              <Arranger
+let stable = true;
+
+export default ({ setState, state, setSQON, sqon, savedSetsContext, history, ...props }) => (
+  <Row css={searchStyles}>
+    <SplitPane
+      split="vertical"
+      minSize={50}
+      defaultSize={state.panelSize}
+      onChange={panelSize => {
+        setState({ panelSize });
+      }}
+      onDragStarted={() => (stable = false)}
+      onDragFinished={() => (stable = true)}
+    >
+      <Col className="aggregations-wrapper">
+        <Component shouldUpdate={() => stable}>
+          {() => (
+            <>
+              <Quicksearch {...{ ...props, setSQON }} />
+              <Aggregations
                 {...props}
-                projectId={props.version}
-                render={props => {
-                  return (
-                    <Row css={searchStyles}>
-                      <Col className="aggregations-wrapper">
-                        <Quicksearch {...{ ...props, setSQON }} />
-                        <Aggregations
-                          {...props}
-                          sqon={sqon}
-                          setSQON={setSQON}
-                          index={props.index}
-                          graphqlField={props.index}
-                          Wrapper={props => <React.Fragment {...props} />}
-                        />
-                      </Col>
-                      <Col className="search-results-wrapper" p={30} flex={1}>
-                        <Row
-                          bg="white"
-                          css={`
-                            height: 185px;
-                            padding: 16px;
-                          `}
-                        >
-                          <PrimarySiteChart sqon={sqon} />
-                          <GrowthChart sqon={sqon} />
-                        </Row>
-                        <Row>
-                          {!sqon && (
-                            <Row
-                              css={`
-                                line-height: 50px;
-                                padding: 0 14px;
-                                background-color: white;
-                                flex: 1;
-                              `}
-                            >
-                              <span
-                                className="sqon-field"
-                                css={`
-                                  font-size: 12px;
-                                  color: #d0d0d0ab;
-                                `}
-                              >
-                                FILTERS
-                              </span>
-                            </Row>
-                          )}
-                          <CurrentSQON
-                            {...props}
-                            sqon={sqon}
-                            setSQON={setSQON}
-                            index={props.index}
-                            graphqlField={props.index}
-                          />
-                        </Row>
-                        <SelectedModelsContext.Consumer>
-                          {selected => {
-                            return (
-                              <Table
-                                {...props}
-                                setSelectedTableRows={selectedRows =>
-                                  selected.setModels({ models: selectedRows })
-                                }
-                                keepSelectedOnPageChange={true}
-                                initalSelectedTableRows={selected.state.models}
-                                loading={savedSetsContext.state.loading || props.loading}
-                                sqon={sqon}
-                                setSQON={setSQON}
-                                onSortedChange={sorted => setState({ sorted })}
-                                alwaysSorted={[{ field: 'name', order: 'asc' }]}
-                                customTypes={{
-                                  entity: props => (
-                                    <TableEntity
-                                      {...props}
-                                      savedSetsContext={savedSetsContext}
-                                      state={state}
-                                      sqon={sqon}
-                                      history={history}
-                                    />
-                                  ),
-                                }}
-                                index={props.index}
-                                graphqlField={props.index}
-                                columnDropdownText="Columns"
-                                fieldTypesForFilter={['text', 'keyword', 'id']}
-                              />
-                            );
-                          }}
-                        </SelectedModelsContext.Consumer>
-                      </Col>
-                    </Row>
-                  );
-                }}
+                sqon={sqon}
+                setSQON={setSQON}
+                index={props.index}
+                graphqlField={props.index}
+                Wrapper={props => <React.Fragment {...props} />}
               />
+            </>
+          )}
+        </Component>
+      </Col>
+      <Col
+        className="search-results-wrapper"
+        p={30}
+        flex={1}
+        css={`
+          width: calc(100vw - ${state.panelSize}px);
+        `}
+      >
+        <Row
+          bg="white"
+          css={`
+            height: 185px;
+            padding: 16px;
+          `}
+        >
+          <Component shouldUpdate={() => stable}>
+            {() => (
+              <>
+                <PrimarySiteChart sqon={sqon} />
+                <GrowthChart sqon={sqon} />
+              </>
             )}
-          </SavedSetsContext.Consumer>
-        )}
-      />
-    )}
-  </Component>
+          </Component>
+        </Row>
+        <Row>
+          {!sqon && (
+            <Row
+              css={`
+                line-height: 50px;
+                padding: 0 14px;
+                background-color: white;
+                flex: 1;
+              `}
+            >
+              <span
+                className="sqon-field"
+                css={`
+                  font-size: 12px;
+                  color: #d0d0d0ab;
+                `}
+              >
+                FILTERS
+              </span>
+            </Row>
+          )}
+          <CurrentSQON
+            {...props}
+            sqon={sqon}
+            setSQON={setSQON}
+            index={props.index}
+            graphqlField={props.index}
+          />
+        </Row>
+        <Component shouldUpdate={() => stable}>
+          {() => (
+            <SelectedModelsContext.Consumer>
+              {selected => {
+                return (
+                  <Table
+                    {...props}
+                    setSelectedTableRows={selectedRows =>
+                      selected.setModels({ models: selectedRows })
+                    }
+                    keepSelectedOnPageChange={true}
+                    initalSelectedTableRows={selected.state.models}
+                    loading={savedSetsContext.state.loading || props.loading}
+                    sqon={sqon}
+                    setSQON={setSQON}
+                    onSortedChange={sorted => setState({ sorted })}
+                    alwaysSorted={[{ field: 'name', order: 'asc' }]}
+                    customTypes={{
+                      entity: props => (
+                        <TableEntity
+                          {...props}
+                          savedSetsContext={savedSetsContext}
+                          state={state}
+                          sqon={sqon}
+                          history={history}
+                        />
+                      ),
+                    }}
+                    index={props.index}
+                    graphqlField={props.index}
+                    columnDropdownText="Columns"
+                    fieldTypesForFilter={['text', 'keyword', 'id']}
+                  />
+                );
+              }}
+            </SelectedModelsContext.Consumer>
+          )}
+        </Component>
+      </Col>
+    </SplitPane>
+  </Row>
 );
---
+++ ui/src/components/SearchWrapper.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Component from 'react-component-component';
+import { Arranger } from '@arranger/components/dist/Arranger';
+import '@arranger/components/public/themeStyles/beagle/beagle.css';
+import Url from 'components/Url';
+import { SavedSetsContext } from 'providers/SavedSets';
+import Search from 'components/Search';
+
+export default props => (
+  <Component initialState={{ sorted: [], panelSize: 300 }}>
+    {state => (
+      <Url
+        render={urlProps => (
+          <SavedSetsContext.Consumer>
+            {savedSetsContext => (
+              <Arranger
+                {...props}
+                projectId={props.version}
+                render={props => (
+                  <Search {...props} {...urlProps} {...state} savedSetsContext={savedSetsContext} />
+                )}
+              />
+            )}
+          </SavedSetsContext.Consumer>
+        )}
+      />
+    )}
+  </Component>
+);
