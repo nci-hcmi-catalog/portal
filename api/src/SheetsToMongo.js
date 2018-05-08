75507e05f4c6ba2c9fcd2ecb3f145f7dae838386
Christine Yu <christine.yu@oicr.on.ca>
2018-05-08 15:38:09 -0400

    /sheets-data endpoint

---
+++ api/src/SheetsToMongo.js
@@ -0,0 +1,68 @@
+import fs from 'fs';
+import { google } from 'googleapis';
+
+export const getAuthClient = () => {
+  const OAuth2Client = google.auth.OAuth2;
+  const TOKEN_PATH = 'token.json';
+
+  const client_id = process.env.CLIENT_ID;
+  const client_secret = process.env.CLIENT_SECRET;
+  const redirect_uris = process.env.REDIRECT_URIS;
+  const oAuth2Client = new OAuth2Client(client_id, client_secret, redirect_uris[0]);
+
+  const token = fs.readFileSync(TOKEN_PATH, 'utf8');
+  oAuth2Client.setCredentials(JSON.parse(token));
+  return oAuth2Client;
+};
+
+export const getSheetData = authClient => {
+  const sheets = google.sheets({ version: 'v4', auth: authClient });
+  return new Promise((resolve, reject) => {
+    sheets.spreadsheets.values.get(
+      {
+        spreadsheetId: process.env.SHEET_ID,
+        range: 'Real Data Collection!A1:AC',
+      },
+      (err, resp) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        const { data  }= resp;
+        const rows = data.values;
+        const asObjects = rows.slice(1).map(r =>
+          rows[0].reduce(
+            (acc, heading, i) => ({
+              ...acc,
+              [heading
+                .toLowerCase()
+                .replace(/ /g, '_')
+                .replace('*', '')]: r[i],
+            }),
+            {},
+          ),
+        );
+        resolve(asObjects);
+      },
+    );
+  });
+};
+
+export const typeToParser = {
+  String: data => (data ? `${data}` : null),
+  Number: data => (isNaN(Number(data)) ? null : Number(data)),
+  Date: data => {
+    const parsed = Date.parse(data);
+    return isNaN(parsed) ? null : parsed;
+  },
+  Boolean: data => {
+    return (
+      {
+        yes: true,
+        no: false,
+        true: true,
+        false: false,
+      }[(data || '').toLowerCase()] || false
+    );
+  },
+};
--- api/src/index.js
+++ api/src/index.js
@@ -5,6 +5,12 @@ import { Server } from 'http';
 import { rainbow } from 'chalk-animation';
 import Arranger from '@arranger/server';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import Model, { ModelSchema } from './schemas/model';
+
+import { getSheetData, typeToParser, getAuthClient } from './SheetsToMongo';
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/test');
 
 const port = process.env.PORT || 5050;
 const app = express();
@@ -14,6 +20,43 @@ app.use(cors());
 
 Arranger({ io }).then(router => {
   app.use(router);
+
+  app.get('/sheets-data', async (req, res) => {
+    const authClient = getAuthClient();
+    getSheetData(authClient)
+      .then(data => res.json(data))
+      .catch(error => res.json({ error: `error reading sheet, ${error}` }));
+  });
+
+  app.get('/sync-mongo', async (req, res) => {
+    const authClient = getAuthClient();
+    getSheetData(authClient)
+      .then(data => {
+        const parsed = data
+          .filter(({ model_name }) => model_name)
+          .map(d =>
+            Object.keys(d).reduce(
+              (acc, key) => ({
+                ...acc,
+                [key]: typeToParser[ModelSchema.paths[key].instance](d[key]),
+              }),
+              {},
+            ),
+          )
+          .filter(Boolean);
+
+        const savePromises = parsed.map(p => {
+          const model = new Model(p);
+          return model.save();
+        });
+
+        Promise.all(savePromises)
+          .then(docs => res.json({ docs }))
+          .catch(error => error);
+      })
+      .catch(error => res.json({ error: `error reading sheet, ${error}` }));
+  });
+
   http.listen(port, async () => {
     rainbow(`⚡️ Listening on port ${port} ⚡️`);
   });
