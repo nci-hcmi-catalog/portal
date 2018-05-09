0c3a0ba7ac656f86919f51cdff47394679e660a8
Christine Yu <christine.yu@oicr.on.ca>
2018-05-09 10:06:14 -0400

    remove check for no set if no sqon
    Closes #52

--- ui/src/components/TableEntity.js
+++ ui/src/components/TableEntity.js
@@ -5,28 +5,24 @@ export default ({ sqon, savedSetsContext, state, value, history }) => (
   <div
     className="clickable"
     onClick={async () => {
-      if (sqon) {
-        const { setId } = await savedSetsContext.createSet({
-          sqon,
-          sort: [...state.sorted, { id: 'name', desc: false }].map(({ id, desc }) => ({
-            field: id,
-            order: desc ? 'desc' : 'asc',
-          })),
-        });
-        if (setId) {
-          history.push({
-            pathname: `/model/${value}`,
-            search: stringify({
-              sqon: JSON.stringify({
-                op: 'in',
-                content: { field: 'setId', value: setId },
-              }),
+      const { setId } = await savedSetsContext.createSet({
+        sqon,
+        sort: [...state.sorted, { id: 'name', desc: false }].map(({ id, desc }) => ({
+          field: id,
+          order: desc ? 'desc' : 'asc',
+        })),
+      });
+      if (setId) {
+        history.push({
+          pathname: `/model/${value}`,
+          search: stringify({
+            sqon: JSON.stringify({
+              op: 'in',
+              content: { field: 'setId', value: setId },
             }),
-          });
-        }
-        return;
+          }),
+        });
       }
-      history.push(`/model/${value}`);
     }}
   >
     {value}
