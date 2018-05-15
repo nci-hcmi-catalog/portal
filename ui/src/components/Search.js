a774ecf8204af0658755d97e72f689668f896237
Alex Wilmer <djazium@gmail.com>
2018-05-15 16:05:36 -0400

    🐛 remove PassChildren wrapper (#66)

--- ui/src/components/Search.js
+++ ui/src/components/Search.js
@@ -12,7 +12,6 @@ import { Row, Col } from 'theme/system';
 import { SelectedModelsContext } from 'providers/SelectedModels';
 
 let stable = true;
-let PassChildren = ({ children }) => <>{children}</>;
 
 export default ({ setState, state, setSQON, sqon, savedSetsContext, history, ...props }) => (
   <Row css={searchStyles}>
@@ -38,7 +37,6 @@ export default ({ setState, state, setSQON, sqon, savedSetsContext, history, ...
                 setSQON={setSQON}
                 index={props.index}
                 graphqlField={props.index}
-                Wrapper={PassChildren}
               />
             </>
           )}
