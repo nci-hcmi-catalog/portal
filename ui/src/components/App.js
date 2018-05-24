da8e37d613c622fb9cd20a1fbafb32929ec7c52e
Christine <christine.tc.yu@gmail.com>
2018-05-24 11:46:50 -0400

    update react - fixes context api getting stuck (#82)
    Closes #65

--- ui/src/components/App.js
+++ ui/src/components/App.js
@@ -20,80 +20,85 @@ import RootProvider from 'providers/RootProvider';
 import { ModalStateContext } from 'providers/ModalState';
 import WarningModal from 'components/modals/WarningModal';
 
+// issue with react-router and react context provider workaround:
+// Router and Context must be rendered in seperate render calls, else
+// Router does not detect route changes
+// https://github.com/ReactTraining/react-router/issues/6072
+const ProvidedRoutes = () => (
+  <ModalStateContext.Consumer>
+    {modalState => (
+      <Component
+        initialState={{
+          version: globals.VERSION,
+        }}
+        didMount={() => {
+          if (!localStorage.getItem(globals.SEEN_WARNING_KEY)) {
+            modalState.setModal({ component: <WarningModal modalState={modalState} /> });
+          }
+        }}
+      >
+        {({ state }) => (
+          <>
+            <Header />
+            <Switch>
+              <Route
+                path="/"
+                exact
+                render={() => <Search version={state.version} index="models" />}
+              />
+              <Route
+                path="/arranger"
+                render={({ match }) => <ArrangerDashboard basename={match.url} />}
+              />
+              <Route
+                path="/admin"
+                render={() => (
+                  <Col>
+                    <Row>
+                      <Row p={15}>
+                        <Link to="/">« Back to List View</Link>
+                      </Row>
+                      <Row flex={1} p={15}>
+                        HCMI Searchable Catalog Administration
+                      </Row>
+                      <Row p={15}>Logout</Row>
+                    </Row>
+                    <Row>
+                      <AdminNav />
+                      <Route path="/admin/manage_users" render={() => <Row p={15}>Users</Row>} />
+                      <Route
+                        path="/admin/single_model_upload"
+                        render={() => <Row p={15}>single model upload</Row>}
+                      />
+                      <Route
+                        path="/admin/bulk_model_upload"
+                        render={() => <Row p={15}>bulk model upload</Row>}
+                      />
+                      <Route
+                        path="/admin/manage_models"
+                        render={() => <Row p={15}>manage models</Row>}
+                      />
+                    </Row>
+                  </Col>
+                )}
+              />
+              <Route
+                path="/model/:modelName"
+                render={({ match }) => <Model modelName={match.params.modelName} />}
+              />
+            </Switch>
+          </>
+        )}
+      </Component>
+    )}
+  </ModalStateContext.Consumer>
+);
+
 export default () => (
   <ThemeProvider theme={theme}>
     <RootProvider>
       <Router>
-        <ModalStateContext.Consumer>
-          {modalState => (
-            <Component
-              initialState={{
-                version: globals.VERSION,
-              }}
-              didMount={() => {
-                if (!localStorage.getItem(globals.SEEN_WARNING_KEY)) {
-                  modalState.setModal({ component: <WarningModal modalState={modalState} /> });
-                }
-              }}
-            >
-              {({ state }) => (
-                <>
-                  <Header />
-                  <Switch>
-                    <Route
-                      path="/"
-                      exact
-                      render={() => <Search version={state.version} index="models" />}
-                    />
-                    <Route
-                      path="/arranger"
-                      render={({ match }) => <ArrangerDashboard basename={match.url} />}
-                    />
-                    <Route
-                      path="/admin"
-                      render={() => (
-                        <Col>
-                          <Row>
-                            <Row p={15}>
-                              <Link to="/">« Back to List View</Link>
-                            </Row>
-                            <Row flex={1} p={15}>
-                              HCMI Searchable Catalog Administration
-                            </Row>
-                            <Row p={15}>Logout</Row>
-                          </Row>
-                          <Row>
-                            <AdminNav />
-                            <Route
-                              path="/admin/manage_users"
-                              render={() => <Row p={15}>Users</Row>}
-                            />
-                            <Route
-                              path="/admin/single_model_upload"
-                              render={() => <Row p={15}>single model upload</Row>}
-                            />
-                            <Route
-                              path="/admin/bulk_model_upload"
-                              render={() => <Row p={15}>bulk model upload</Row>}
-                            />
-                            <Route
-                              path="/admin/manage_models"
-                              render={() => <Row p={15}>manage models</Row>}
-                            />
-                          </Row>
-                        </Col>
-                      )}
-                    />
-                    <Route
-                      path="/model/:modelName"
-                      render={({ match }) => <Model modelName={match.params.modelName} />}
-                    />
-                  </Switch>
-                </>
-              )}
-            </Component>
-          )}
-        </ModalStateContext.Consumer>
+        <ProvidedRoutes />
       </Router>
       <Modal />
     </RootProvider>
