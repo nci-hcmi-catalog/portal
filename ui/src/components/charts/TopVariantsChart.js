518093fe460fc16df6db7661e8caf825e38246b2
Christine Yu <christine.yu@oicr.on.ca>
2018-06-05 15:21:33 -0400

    keep colours stable by variant

--- ui/src/components/charts/TopVariantsChart.js
+++ ui/src/components/charts/TopVariantsChart.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { range } from 'lodash';
+import { range, isEqual } from 'lodash';
 import { ResponsiveBar } from '@nivo/bar';
 import AggregationQuery from 'components/Queries/AggregationQuery';
 import { Col } from 'theme/system';
 import theme from 'theme';
 import { toggleSQON } from '@arranger/components/dist/SQONView/utils';
 import { ChartTooltip } from './';
+import Component from 'react-component-component';
 
 const yGridSizes = [10, 100, 1000];
 
@@ -18,102 +19,136 @@ export default ({ sqon, setSQON }) => (
     `}
   >
     <AggregationQuery sqon={sqon} field="variants__name">
-      {({
-        state,
-        top10 = (state.buckets || [])
-          .sort((a, b) => b.doc_count - a.doc_count)
-          .slice(0, 10)
-          .map((bucket, i) => ({
-            ...bucket,
-            color: theme.palette[i],
-          })),
-        largestCount = top10.reduce(
-          (largest, { doc_count }) => (doc_count > largest ? doc_count : largest),
-          0,
-        ),
-        yGridStepSize = yGridSizes.reduce(
-          (gridSize, possible) => (largestCount > possible ? possible : gridSize),
-          10,
-        ),
-        yGridValues = range(
-          0,
-          largestCount + yGridStepSize * 2, //ensure that largest will be in the range
-          yGridStepSize,
-        ),
-      }) => {
-        return state.loading ? (
-          'loading'
-        ) : (
-          <>
-            <span
-              className="sqon-field"
-              css={`
-                font-size: 12px;
-              `}
-            >
-              Top {top10.length} Variants in {state.total} Models
-              {console.log(yGridStepSize)}
-              {console.log(yGridValues)}
-            </span>
-            <ResponsiveBar
-              margin={{
-                top: 25,
-                right: 15,
-                bottom: 38,
-                left: 60,
-              }}
-              data={top10}
-              enableLabel={false}
-              padding={0.4}
-              indexBy="key"
-              keys={['doc_count']}
-              colorBy={data => {
-                return (data || { data: { color: 'white' } }).data.color;
-              }}
-              axisBottom={{
-                orient: 'bottom',
-                tickSize: 3,
-                tickPadding: 2,
-                tickRotation: -45,
-                legendOffset: 50,
-                format: value => (value.length >= 4 ? `${value.substring(0, 4)}\u2026` : value),
-              }}
-              axisLeft={{
-                orient: 'left',
-                tickSize: 5,
-                tickPadding: 5,
-                tickRotation: 0,
-                legendPosition: 'center',
-                legendOffset: -45,
-                legend: '# Models',
-                tickValues: yGridValues,
-              }}
-              enableGridY={true}
-              gridYValues={yGridValues}
-              theme={theme.chart}
-              tooltip={({ value, data }) => ChartTooltip({ value, label: data.key })}
-              isInteractive={true}
-              onClick={data =>
-                setSQON(
-                  toggleSQON(
-                    {
-                      op: 'and',
-                      content: [
-                        {
-                          op: 'in',
-                          content: {
-                            field: 'variants.name',
-                            value: [].concat(data.data.key || []),
+      {({ state: aggState }) => {
+        return (
+          <Component
+            top10={(aggState.buckets || []).sort((a, b) => b.doc_count - a.doc_count).slice(0, 10)}
+            initialState={{ coloredTop10: [] }}
+            loading={aggState.loading}
+            shouldUpdate={({ props, nextProps, state, nextState }) => {
+              return (
+                !isEqual(props.top10, nextProps.top10) ||
+                !isEqual(state.coloredTop10, nextState.coloredTop10)
+              );
+            }}
+            didUpdate={({ setState, props, prevState }) => {
+              const byKey = prevState.coloredTop10.reduce(
+                (acc, { key, color }) => ({ ...acc, [key]: color }),
+                {},
+              );
+              const { coloredTop10 } = props.top10.reduce(
+                (acc, bucket) => {
+                  const color = byKey[bucket.key] || acc.palette[0];
+                  const palette = acc.palette.filter(pColor => pColor !== color);
+                  return {
+                    coloredTop10: [
+                      ...acc.coloredTop10,
+                      {
+                        ...bucket,
+                        color,
+                      },
+                    ],
+                    palette,
+                  };
+                },
+                { coloredTop10: [], palette: theme.palette },
+              );
+              setState({
+                coloredTop10,
+              });
+            }}
+          >
+            {({
+              state: { coloredTop10, loading },
+              largestCount = coloredTop10.reduce(
+                (largest, { doc_count }) => (doc_count > largest ? doc_count : largest),
+                0,
+              ),
+              yGridStepSize = yGridSizes.reduce(
+                (gridSize, possible) => (largestCount > possible ? possible : gridSize),
+                10,
+              ),
+              yGridValues = largestCount > 0
+                ? range(
+                    0,
+                    largestCount + yGridStepSize * 2, //ensure that largest will be in the range
+                    yGridStepSize,
+                  )
+                : [],
+            }) => {
+              return loading ? (
+                'loading'
+              ) : (
+                <>
+                  <span
+                    className="sqon-field"
+                    css={`
+                      font-size: 12px;
+                    `}
+                  >
+                    Top {coloredTop10.length} Variants in {aggState.total} Models
+                  </span>
+                  <ResponsiveBar
+                    margin={{
+                      top: 25,
+                      right: 15,
+                      bottom: 38,
+                      left: 60,
+                    }}
+                    data={coloredTop10}
+                    enableLabel={false}
+                    padding={0.4}
+                    indexBy="key"
+                    keys={['doc_count']}
+                    colorBy={data => (data || { data: { color: 'white' } }).data.color}
+                    axisBottom={{
+                      orient: 'bottom',
+                      tickSize: 3,
+                      tickPadding: 2,
+                      tickRotation: -45,
+                      legendOffset: 50,
+                      format: value =>
+                        value.length >= 4 ? `${value.substring(0, 4)}\u2026` : value,
+                    }}
+                    axisLeft={{
+                      orient: 'left',
+                      tickSize: 5,
+                      tickPadding: 5,
+                      tickRotation: 0,
+                      legendPosition: 'center',
+                      legendOffset: -45,
+                      legend: '# Models',
+                      tickValues: yGridValues,
+                    }}
+                    enableGridY={true}
+                    gridYValues={yGridValues}
+                    theme={theme.chart}
+                    tooltip={({ value, data }) => ChartTooltip({ value, label: data.key })}
+                    isInteractive={true}
+                    onClick={data =>
+                      setSQON(
+                        toggleSQON(
+                          {
+                            op: 'and',
+                            content: [
+                              {
+                                op: 'in',
+                                content: {
+                                  field: 'variants.name',
+                                  value: [].concat(data.data.key || []),
+                                },
+                              },
+                            ],
                           },
-                        },
-                      ],
-                    },
-                    sqon,
-                  ),
-                )
-              }
-            />
-          </>
+                          sqon,
+                        ),
+                      )
+                    }
+                  />
+                </>
+              );
+            }}
+          </Component>
         );
       }}
     </AggregationQuery>
