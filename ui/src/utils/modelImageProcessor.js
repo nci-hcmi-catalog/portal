657e75877e57bdaded04f0845ac1274bf9976353
Alex <lepsalex@gmail.com>
2018-06-06 16:22:35 -0400

    apiDataProcessor util func to process empty data to '--'

--- ui/src/utils/modelImageProcessor.js
+++ ui/src/utils/modelImageProcessor.js
@@ -1,5 +1,6 @@
 /**
- *
+ * Processes file hits from models into an array of image objects
+ * if there are images in the file hits.
  * @param {Array} files - File hits from GraphQL query
  * @returns {Array || false} - Returns either the processed images or false
  */
