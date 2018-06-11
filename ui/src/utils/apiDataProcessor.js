2dab4552bc56a8f57a60e0cc5f460df559552ee2
Christine Yu <christine.yu@oicr.on.ca>
2018-06-11 17:43:36 -0400

    :recycle: refactor apiDataProcessor to use extended mapping

--- ui/src/utils/apiDataProcessor.js
+++ ui/src/utils/apiDataProcessor.js
@@ -1,17 +1,4 @@
-/**
- * If there is data just return it, otherwise return
- * the characters "--" - our convention for no-data
- * @param {*} data - Any object, string, array, we are checking for content
- * @param {*} implicitReturn - Returns this instead of data if provided
- * data from api response
- */
-export default function(data, implicitReturn = false) {
-  return !data || data.length === 0 || isEmptyObject(data)
-    ? '--'
-    : implicitReturn
-      ? implicitReturn
-      : data;
-}
+import moment from 'moment';
 
 function isEmptyObject(obj) {
   if (typeof obj !== 'object') {
@@ -24,3 +11,22 @@ function isEmptyObject(obj) {
 
   return true;
 }
+
+const processors = {
+  date: value => moment(value).format('DD/MM/YYYY'),
+  boolean: value => (value ? 'Yes' : 'No'),
+  keyword: value => `${value}`,
+  long: value => value.toLocaleString(),
+  short: value => value.toLocaleString(),
+};
+
+/**
+ * If there is data process it, otherwise return
+ * the characters "--" - our convention for no-data
+ * @param {*} data - Any object, string, array, we are checking for content
+ * @param String displayType - date, boolean, keyword, long, url
+ */
+export default ({ data, type, unit }) =>
+  (type !== 'boolean' && !data) || data.length === 0 || isEmptyObject(data)
+    ? '--'
+    : `${processors[type || 'keyword'](data)}${unit ? ` ${unit}` : ''}`;
