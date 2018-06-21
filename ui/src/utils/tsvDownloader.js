b71be6db227029a0d38d072cc48bfc446277d528
Alex <lepsalex@gmail.com>
2018-06-21 13:09:44 -0400

    Comment on util function

--- ui/src/utils/tsvDownloader.js
+++ ui/src/utils/tsvDownloader.js
@@ -1,5 +1,12 @@
 import { saveAs } from 'filesaver.js';
 
+/**
+ * Given a array of like objects, transform keys to header
+ * row and then output each object values as a row in a tsv
+ * format export with fileName
+ * @param {String} fileName - filename without extension
+ * @param {[Object]} objArr - array of like objects
+ */
 export default function(fileName, objArr) {
   saveAs(
     new Blob(
