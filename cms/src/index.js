2090ff24e3c10496e55082e980e05e9856c864a3
Christine Yu <christine.yu@oicr.on.ca>
2018-06-28 15:11:45 -0400

    :sparkles: cms - only update modified data - npm script for getToken - accept mutliple sheets: take sheet id and tab name in url - add new models that are not found
    Closes #120

--- cms/src/index.js
+++ cms/src/index.js
@@ -4,6 +4,7 @@ import { Server } from 'http';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import Model, { ModelSchema } from './data/schemas/model';
+import { isEqual } from 'lodash';
 
 import { getSheetData, typeToParser, getAuthClient } from './data/import/SheetsToMongo';
 
@@ -14,20 +15,22 @@ const app = express();
 const http = Server(app);
 app.use(cors());
 
-app.get('/sheets-data', async (req, res) => {
+app.get('/sheets-data/:sheetId/:tabName', async (req, res) => {
   const authClient = getAuthClient();
-  getSheetData(authClient)
+  const { sheetId, tabName } = req.params;
+  getSheetData({ authClient, sheetId, tabName })
     .then(data => res.json(data))
     .catch(error =>
       res.json({
-        error: `error reading sheet, ${error}`,
+        error: `error reading sheet ID ${sheetId}, ${error}`,
       }),
     );
 });
 
-app.get('/sync-mongo', async (req, res) => {
+app.get('/sync-mongo/:sheetId/:tabName', async (req, res) => {
   const authClient = getAuthClient();
-  getSheetData(authClient)
+  const { sheetId, tabName } = req.params;
+  getSheetData({ authClient, sheetId, tabName })
     .then(data => {
       const parsed = data
         .filter(({ model_name }) => model_name)
@@ -42,18 +45,32 @@ app.get('/sync-mongo', async (req, res) => {
         )
         .filter(Boolean);
 
-      const savePromises = parsed.map(p => {
-        const model = new Model(p);
-        return model.save();
+      const savePromises = parsed.map(async p => {
+        const prevModel = await Model.findOne(
+          { model_name: p.model_name },
+          { _id: false, __v: false }, //omit mongoose generated fields
+        );
+        if (prevModel) {
+          if (!isEqual(prevModel._doc, p)) {
+            return Model.findOneAndUpdate({ model_name: p.model_name }, p, {
+              upsert: true,
+              new: true,
+            });
+          }
+          return new Promise(resolve => resolve({})); //no fields modified, do nothing
+        } else {
+          const newModel = new Model(p);
+          return newModel.save();
+        }
       });
 
       Promise.all(savePromises)
         .then(docs =>
           res.json({
-            docs,
+            docs: docs.filter(d => !!Object.keys(d).length),
           }),
         )
-        .catch(error => error);
+        .catch(error => res.json({ error }));
     })
     .catch(error =>
       res.json({
