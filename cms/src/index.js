d2bb63216212eab4753d3528e137c8c02f8518d8
Rahul Verma <Rahul.Verma@oicr.on.ca>
2018-07-11 13:53:24 -0400

    :recycle: Refactors express routing

--- cms/src/index.js
+++ cms/src/index.js
@@ -3,15 +3,12 @@ import express from 'express';
 import { Server } from 'http';
 import cors from 'cors';
 import mongoose from 'mongoose';
-import Model, { ModelSchema } from './data/schemas/model';
-import { isEqual } from 'lodash';
+import Model from './schemas/model';
 import bodyParser from 'body-parser';
 import methodOverride from 'method-override';
 import restify from 'express-restify-mongoose';
 import morgan from 'morgan';
-
-import { getSheetData, typeToParser, NAtoNull, getAuthClient } from './data/import/SheetsToMongo';
-
+import { data_sync_router } from './routes/sync-data';
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/test');
 
 const port = process.env.PORT || 8080;
@@ -23,98 +20,10 @@ app.use(methodOverride());
 app.use(morgan('combined'));
 
 restify.serve(router, Model);
-
+app.use('/', data_sync_router);
 app.use(router);
 app.use(cors());
 const http = Server(app);
-
-app.get('/sheets-data/:sheetId/:tabName', async (req, res) => {
-  const authClient = getAuthClient();
-  const { sheetId, tabName } = req.params;
-  getSheetData({
-    authClient,
-    sheetId,
-    tabName,
-  })
-    .then(data => res.json(data))
-    .catch(error =>
-      res.json({
-        error: `error reading sheet ID ${sheetId}, ${error}`,
-      }),
-    );
-});
-
-app.get('/sync-mongo/:sheetId/:tabName', async (req, res) => {
-  const authClient = getAuthClient();
-  const { sheetId, tabName } = req.params;
-  getSheetData({
-    authClient,
-    sheetId,
-    tabName,
-  })
-    .then(data => {
-      const parsed = data
-        .filter(({ model_name }) => model_name)
-        .map(d =>
-          Object.keys(d).reduce(
-            (acc, key) => ({
-              ...acc,
-              [key]: typeToParser[ModelSchema.paths[key].instance](NAtoNull(d[key])),
-            }),
-            {},
-          ),
-        )
-        .filter(Boolean);
-
-      const savePromises = parsed.map(async p => {
-        const prevModel = await Model.findOne(
-          {
-            model_name: p.model_name,
-          },
-          {
-            _id: false,
-            __v: false,
-          }, //omit mongoose generated fields
-        );
-        if (prevModel) {
-          if (!isEqual(prevModel._doc, p)) {
-            return Model.findOneAndUpdate(
-              {
-                model_name: p.model_name,
-              },
-              p,
-              {
-                upsert: true,
-                new: true,
-                runValidators: true,
-              },
-            );
-          }
-          return new Promise(resolve => resolve({})); //no fields modified, do nothing
-        } else {
-          const newModel = new Model(p);
-          return newModel.save();
-        }
-      });
-
-      Promise.all(savePromises)
-        .then(docs =>
-          res.json({
-            docs: docs.filter(d => !!Object.keys(d).length),
-          }),
-        )
-        .catch(error =>
-          res.json({
-            error,
-          }),
-        );
-    })
-    .catch(error =>
-      res.json({
-        error: `error reading sheet, ${error}`,
-      }),
-    );
-});
 
 http.listen(port, async () => {
   console.log(`⚡️ Listening on port ${port} ⚡️`);
