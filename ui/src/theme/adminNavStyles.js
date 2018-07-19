3d1b2f8b3b096b5379feafa033a0010ca259bc76
Alex <lepsalex@gmail.com>
2018-07-19 13:46:47 -0400

    Complete nav on-state

--- ui/src/theme/adminNavStyles.js
+++ ui/src/theme/adminNavStyles.js
@@ -17,9 +17,13 @@ export default css`
   display: flex;
   justify-content: space-between;
   padding: 0 30px;
+  border-top: 6px solid white;
+  border-bottom: 6px solid white;
 
   .nav {
     a {
+      display: inline-block;
+      position: relative;
       color: white;
       text-decoration: none;
       font-size: 14px;
@@ -34,6 +38,19 @@ export default css`
 
       &.active {
         background-color: ${navOnState};
+
+        &::after {
+          display: block;
+          position: absolute;
+          left: calc(50% - 7px);
+          bottom: 0px;
+          content: '';
+          width: 0;
+          height: 0;
+          border-left: 7px solid transparent;
+          border-right: 7px solid transparent;
+          border-bottom: 7px solid white;
+        }
       }
     }
   }
