b3dd3b2bec7083e1b92e9adee8eec010f2d8b871
Alex <lepsalex@gmail.com>
2018-07-24 11:12:54 -0400

    Importing yup schema from cms package succesfully

--- ui/src/components/admin/ModelUploadSingle.js
+++ ui/src/components/admin/ModelUploadSingle.js
@@ -1,4 +1,5 @@
 import React from 'react';
 import { Row } from 'theme/system';
+import modelValidation from '@hcmi-portal/cms/src/validation/model';
 
 export default () => <Row p={15}>Model Upload Single</Row>;
