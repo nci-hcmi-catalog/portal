51a110bebd69be06de7420f71a9842211bd3b71e
Rahul Verma <Rahul.Verma@oicr.on.ca>
2018-07-25 17:51:04 -0400

    :art: Adds Model Management header

--- ui/src/components/admin/index.js
+++ ui/src/components/admin/index.js
@@ -3,17 +3,17 @@ import { Row, Col } from 'theme/system';
 import { Route } from 'react-router-dom';
 
 import AdminNav from './AdminNav';
-import ModelsManage from './ModelsManage';
+import ModelsManager from './ModelsManager';
 import ModelUploadSingle from './ModelUploadSingle';
 import UsersManage from './UsersManage';
 
 export default ({ location }) => (
   <Col>
-    <AdminNav location={location} />
+    <AdminNav location={location} />{' '}
     <Row>
-      <Route path="/admin/manage-users" render={() => <UsersManage />} />
-      <Route path="/admin/manage-models" render={() => <ModelsManage />} />
-      <Route path="/admin/single-model-upload" render={() => <ModelUploadSingle />} />
-    </Row>
+      <Route path="/admin/manage-users" render={() => <UsersManage />} />{' '}
+      <Route path="/admin/manage-models" render={() => <ModelsManager />} />{' '}
+      <Route path="/admin/single-model-upload" render={() => <ModelUploadSingle />} />{' '}
+    </Row>{' '}
   </Col>
 );
