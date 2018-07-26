c9b1b799ff7dccc1cc944aa3cc812fd4b1e61e98
Alex <lepsalex@gmail.com>
2018-07-26 16:23:03 -0400

    ModelNav active tab styles

--- ui/src/theme/adminModelStyles.js
+++ ui/src/theme/adminModelStyles.js
@@ -6,8 +6,10 @@ import { AdminContent } from 'theme/adminStyles';
 
 const {
   fonts: { libreFranklin },
-  keyedPalette: { brandPrimary, white },
+  keyedPalette: { brandPrimary, porcelain, white },
 } = base;
+
+const borderColour = porcelain;
 
 export const AdminModelNav = styled(Col)`
   width: 164px;
@@ -16,6 +18,15 @@ export const AdminModelNav = styled(Col)`
 
 const activeNavItem = css`
   background: ${white};
+  border: solid 1px ${borderColour};
+  border-right-color: ${white};
+  border-top-left-radius: 10px;
+  border-bottom-left-radius: 10px;
+  /* Extending it over 1 pixel to match design (cover other border) */
+  z-index: 2;
+  position: relative;
+  width: 165px;
+  label: admin-model-nav-item-active;
 `;
 
 export const NavItem = styled('div')`
@@ -30,11 +41,13 @@ export const NavItem = styled('div')`
   text-align: left;
   color: ${brandPrimary};
   text-transform: uppercase;
-  ${props => props.active && activeNavItem};
+  cursor: pointer;
   label: admin-model-nav-item;
+  ${props => props.active && activeNavItem};
 `;
 
 export const AdminModelContent = styled(AdminContent)`
   width: calc(100% - 164px);
+  z-index: 1;
   label: admin-model-content;
 `;
