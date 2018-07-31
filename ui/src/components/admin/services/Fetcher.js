491e18b841237a1c24dee241324d3095742f0154
Rahul Verma <Rahul.Verma@oicr.on.ca>
2018-07-31 15:48:30 -0400

    :bug: Renders fetcher children

--- ui/src/components/admin/services/Fetcher.js
+++ ui/src/components/admin/services/Fetcher.js
@@ -2,30 +2,34 @@ import React from 'react';
 import Component from 'react-component-component';
 import axios from 'axios';
 
-const fetchData = async({url, data, method}) => {
-  return await axios({url, data, method});
+const fetchData = async ({ url, data, method }) => {
+  return await axios({ url, data, method });
 };
 
-export const get = async({url, params}) => {
-  return await fetchData({url, params, method: 'get'});
+export const get = async ({ url, params }) => {
+  return await fetchData({ url, params, method: 'get' });
 };
 
-export const post = async({url, data}) => {
-  return await fetchData({url, params, method: 'post'})
+export const post = async ({ url, data }) => {
+  return await fetchData({ url, data, method: 'post' });
 };
 
-export const Fetcher = ({url, data, method}) => (
+export const Fetcher = ({ url, data, method, children }) => (
   <Component
     initialState={{
-    isLoading: true,
-    data: null,
-    error: null
-  }}
-    didMount={async({setState}) => {
-    try {
-      const response = await fetchData({url, data, method});
-      setState(state => ({isLoading: false, data: response.data, error: null}));
-    } catch (err) {
-      setState(state => ({isLoading: false, data: null, error: err}));
-    }
-  }}/>
+      isLoading: true,
+      data: null,
+      error: null,
+    }}
+    didMount={async ({ setState }) => {
+      try {
+        const response = await fetchData({ url, data, method });
+        setState(() => ({ isLoading: false, data: response.data, error: null }));
+      } catch (err) {
+        setState(() => ({ isLoading: false, data: null, error: err }));
+      }
+    }}
+  >
+    {({ state }) => children(state)}
+  </Component>
+);
