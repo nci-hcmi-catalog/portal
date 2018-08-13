92dd9de779b7baf4258baec372f6b333f3324a3a
Alex <lepsalex@gmail.com>
2018-08-13 15:16:43 -0400

    Admin header data conditions

--- ui/src/components/admin/Model/ModelSingleHeader.js
+++ ui/src/components/admin/Model/ModelSingleHeader.js
@@ -42,35 +42,39 @@ const modelStatus = (data = null) => {
   }
 
   // Additional statuses here
+
+  // Temp
+  return <SmallPill warning>Unsaved Changes</SmallPill>;
 };
 
-const modelMoreOptions = (data = null) => (
-  <Popup
-    trigger={
-      <Pill secondary>
-        <AdminModelMoreOptionsIcon css={'margin: 0;'} width={15} height={3} />
-      </Pill>
-    }
-    position="bottom right"
-    offset={0}
-    on="click"
-    closeOnDocumentClick
-    mouseLeaveDelay={300}
-    mouseEnterDelay={0}
-    contentStyle={{
-      padding: '0px',
-      border: 'none',
-      borderRadius: '10px',
-      width: 'max-content',
-    }}
-    arrow={false}
-  >
-    <ActionsMenu>
-      <ActionsMenuItem>Publish</ActionsMenuItem>
-      <ActionsMenuItem>Delete</ActionsMenuItem>
-    </ActionsMenu>
-  </Popup>
-);
+const modelMoreOptions = (data = null) =>
+  data && (
+    <Popup
+      trigger={
+        <Pill secondary marginLeft="10px">
+          <AdminModelMoreOptionsIcon css={'margin: 0;'} width={15} height={3} />
+        </Pill>
+      }
+      position="bottom right"
+      offset={0}
+      on="click"
+      closeOnDocumentClick
+      mouseLeaveDelay={300}
+      mouseEnterDelay={0}
+      contentStyle={{
+        padding: '0px',
+        border: 'none',
+        borderRadius: '10px',
+        width: 'max-content',
+      }}
+      arrow={false}
+    >
+      <ActionsMenu>
+        <ActionsMenuItem>Publish</ActionsMenuItem>
+        <ActionsMenuItem>Delete</ActionsMenuItem>
+      </ActionsMenu>
+    </Popup>
+  );
 
 const isFormReady = errors => Object.keys(errors).length === 0;
 
@@ -85,7 +89,7 @@ export default ({ modelName }) => (
       <AdminHeader>
         <AdminHeaderBlock>
           {headerText(modelName, error)}
-          {modelStatus(response.data || null)}
+          {modelStatus(response || null)}
         </AdminHeaderBlock>
         <AdminHeaderBlock>
           <ModelHeaderBackLink to={manageModelsUrlBase}>
@@ -94,10 +98,10 @@ export default ({ modelName }) => (
           <Pill disabled={!isReadyToPublish} marginLeft="21px" marginRight="10px">
             <AdminModelPublishIcon css={'margin-right: 10px;'} height={16} width={15} />Publish
           </Pill>
-          <Pill primary disabled={!isReadyToSave} marginRight="10px">
+          <Pill primary disabled={!isReadyToSave}>
             <AdminModelSaveIcon css={'margin-right: 8px;'} height={14} width={14} />Save
           </Pill>
-          {modelMoreOptions(response.data || null)}
+          {modelMoreOptions(response || null)}
         </AdminHeaderBlock>
       </AdminHeader>
     )}
