2ee6872a62917749f3c185bf130aa9d9de9dd798
Alex <lepsalex@gmail.com>
2018-09-25 12:17:02 -0400

    Feat/model manager actions (#298)
    * WIP Actions
    
    * Model Table publish and unpublish
    
    * Model Table delete

---
+++ cms/src/routes/action.js
@@ -0,0 +1,57 @@
+// @ts-check
+
+import express from 'express';
+import Model from '../schemas/model';
+import publishValidation from '../validation/model';
+import { runYupValidators, modelStatus } from '../helpers';
+import { indexOneToES } from '../services/elastic-search/publish';
+import { unpublishOneFromES } from '../services/elastic-search/unpublish';
+
+const actionRouter = express.Router();
+
+actionRouter.post('/publish/:name', async (req, res) => {
+  const { name } = req.params;
+  Model.find({
+    name,
+  })
+    .then(models => runYupValidators(publishValidation, models))
+    .then(() => indexOneToES({ name }))
+    .then(() =>
+      Model.updateOne(
+        {
+          name,
+        },
+        { status: modelStatus.published },
+      ),
+    )
+    .then(() => res.json({ success: `${name} has been successfully published` }))
+    .catch(error =>
+      res.status(500).json({
+        error: error,
+      }),
+    );
+});
+
+actionRouter.post('/unpublish/:name', async (req, res) => {
+  const { name } = req.params;
+  Model.find({
+    name,
+  })
+    .then(() => unpublishOneFromES(name))
+    .then(() =>
+      Model.updateOne(
+        {
+          name,
+        },
+        { status: modelStatus.unpublished },
+      ),
+    )
+    .then(() => res.json({ success: `${name} has been successfully unpublished` }))
+    .catch(error =>
+      res.status(500).json({
+        error: error,
+      }),
+    );
+});
+
+export default actionRouter;
--- cms/src/routes/index.js
+++ cms/src/routes/index.js
@@ -1,2 +1,3 @@
 export { default as bulkRouter } from './bulk';
 export { default as imagesRouter } from './images';
+export { default as actionRouter } from './action';
