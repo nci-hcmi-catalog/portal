53be2dfcaffe68d4c8c9fe50277d6f754ccea73e
Rahul Verma <Rahul.Verma@oicr.on.ca>
2018-09-25 14:22:37 -0400

    :construction: Adds User mgmt layout

--- ui/src/components/admin/UsersManager/UsersManager.js
+++ ui/src/components/admin/UsersManager/UsersManager.js
@@ -1,4 +1,42 @@
 import React from 'react';
-import { Row } from 'theme/system';
 
-export default () => <Row p={15}>Users</Row>;
+import { NotificationToaster } from '../Notifications';
+import UserIcon from '../../../icons/PatientIcon';
+import AdminPlusIcon from '../../../icons/AdminPlusIcon';
+import { AdminContainer, AdminHeader, AdminHeaderH1, AdminHeaderBlock } from 'theme/adminStyles';
+import { Pill } from 'theme/adminControlsStyles';
+import { Table } from 'theme/adminTableStyles';
+import config from '../config';
+import ModelManagerProvider, { ModelManagerContext } from '../ModelsManager/ModelManagerController';
+import ModelManagerTable from '../ModelsManager/ModelManagerTable';
+
+const content = () => {
+  return (
+    <ModelManagerProvider cmsBase={config.urls.cmsBase} baseUrl={`${config.urls.cmsBase}/User`}>
+      <AdminContainer>
+        <NotificationToaster />
+        <AdminHeader>
+          <AdminHeaderH1>
+            <UserIcon
+              height={35}
+              width={35}
+              css={`
+                margin-right: 13px;
+              `}
+            />User Management
+          </AdminHeaderH1>
+          <AdminHeaderBlock>
+            <Pill primary marginRight="10px" onClick={() => {}}>
+              <AdminPlusIcon width={16} height={16} css={'margin-right: 9px;'} />Add A User
+            </Pill>
+          </AdminHeaderBlock>
+        </AdminHeader>
+        <Table>
+          <ModelManagerTable />
+        </Table>
+      </AdminContainer>
+    </ModelManagerProvider>
+  );
+};
+
+export default content;
