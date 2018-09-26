9f1d7bff5e2c75acd08d1ba18bb2c279b4077a74
Alex <lepsalex@gmail.com>
2018-09-26 16:11:20 -0400

    Refactored this for better support of nested paths (variant table)

--- ui/src/components/admin/helpers/tableActions.js
+++ ui/src/components/admin/helpers/tableActions.js
@@ -1,19 +1,58 @@
 // @ts-check
 
-import { xor } from 'lodash';
+import { xor, get } from 'lodash';
 
-export const generateTableActions = (state, setState, data) => ({
-  onPageChange: newPage => setState({ page: newPage, isLoading: true }),
-  onFilterValueChange: newValue => setState({ filterValue: newValue }),
-  onPageSizeChange: newValue => setState({ page: 0, pageSize: newValue, isLoading: true }),
-  toggleSelection: id => setState({ ...state, selection: xor(state.selection, [id]) }),
+const nestedStateResolver = (state, update, path) =>
+  path
+    ? {
+        ...state,
+        [path]: {
+          ...state[path],
+          ...update,
+        },
+      }
+    : update;
+
+export const generateTableActions = (setState, data, nestedStatePath = false) => ({
+  onPageChange: newPage =>
+    setState(state =>
+      nestedStateResolver(state, { page: newPage, isLoading: true }, nestedStatePath),
+    ),
+  onFilterValueChange: newValue =>
+    setState(state => nestedStateResolver(state, { filterValue: newValue }, nestedStatePath)),
+  onPageSizeChange: newValue =>
+    setState(state =>
+      nestedStateResolver(state, { page: 0, pageSize: newValue, isLoading: true }, nestedStatePath),
+    ),
+  toggleSelection: id =>
+    setState(state =>
+      nestedStateResolver(
+        state,
+        {
+          selection: xor(
+            get(state, nestedStatePath ? `${nestedStatePath}.selection` : 'selection'),
+            [id],
+          ),
+        },
+        nestedStatePath,
+      ),
+    ),
   toggleAll: () => {
     const ids = data.map(({ _id }) => _id);
 
-    return setState({
-      ...state,
-      selection: state.selection.length === ids.length ? [] : ids,
-      selectAll: !state.selectAll,
-    });
+    return setState(state =>
+      nestedStateResolver(
+        state,
+        {
+          selection:
+            get(state, nestedStatePath ? `${nestedStatePath}.selection` : 'selection').length ===
+            ids.length
+              ? []
+              : ids,
+          selectAll: !get(state, nestedStatePath ? `${nestedStatePath}.selectAll` : 'selectAll'),
+        },
+        nestedStatePath,
+      ),
+    );
   },
 });
