7a8558939d7ee98fb6caddc4b6eb25e3181c04b0
Alex <lepsalex@gmail.com>
2018-09-28 12:13:33 -0400

    Front-end for last-update date

--- ui/src/components/LastUpdatedDate.js
+++ ui/src/components/LastUpdatedDate.js
@@ -1,6 +1,25 @@
 import React from 'react';
+import moment from 'moment';
 import LastUpdatedDateQuery from 'components/queries/LastUpdatedDateQuery';
 
 export default () => (
-  <LastUpdatedDateQuery>{({ state: { date } }) => <div>{date}</div>}</LastUpdatedDateQuery>
+  <LastUpdatedDateQuery>
+    {({ state: { date } }) => (
+      <div
+        css={`
+          border: 0 !important;
+          font-size: 12px;
+          font-weight: normal;
+          font-style: normal;
+          font-stretch: normal;
+          line-height: 2.17;
+          letter-spacing: normal;
+          text-align: center;
+          color: #64666a;
+        `}
+      >
+        Updated: {moment(date).format('MMMM D, YYYY')}
+      </div>
+    )}
+  </LastUpdatedDateQuery>
 );
