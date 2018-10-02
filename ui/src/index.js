66138ffe3c3745a31d92ee8098ad3aff157ee296
Alex <lepsalex@gmail.com>
2018-10-02 11:38:06 -0400

    Date save 1 day behind #324

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import moment from 'moment-timezone';
 import './index.css';
 import App from './components/App';
 
+// Set global timezone to UTC
+moment.tz.setDefault('UTC');
+
+// Render app
 ReactDOM.render(<App />, document.getElementById('root'));
