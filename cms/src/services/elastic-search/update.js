1c25042d1c966561828ef33faeb62d6fcd83724a
Alex <lepsalex@gmail.com>
2018-10-02 13:48:44 -0400

    Occured => Occurred

--- cms/src/services/elastic-search/update.js
+++ cms/src/services/elastic-search/update.js
@@ -16,6 +16,6 @@ export default () =>
     .catch(err =>
       // Catch here as we do not want an error here to block execution of the app
       console.error(
-        `Error creating a new update in the ${index} index: ${err || 'Unknown error has occured.'}`,
+        `Error creating a new update in the ${index} index: ${err || 'Unknown error has occurred.'}`,
       ),
     );
