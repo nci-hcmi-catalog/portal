3099df348c3e4eb4bea86b7fffd8aecd220e8468
Rahul Verma <Rahul.Verma@oicr.on.ca>
2018-10-04 14:02:24 -0400

    :construction: Combines User info and logout into single control

--- ui/src/components/admin/services/LoggedInUser.js
+++ ui/src/components/admin/services/LoggedInUser.js
@@ -2,7 +2,10 @@ import React from 'react';
 import config from '../config';
 import { Fetcher } from './Fetcher';
 import UserIcon from 'icons/UserIcon';
+import CollapsibleArrow from 'icons/CollapsibleArrow';
 import { User, Pill } from 'theme/adminNavStyles';
+import Popup from 'reactjs-popup';
+import Component from 'react-component-component';
 
 export const LoggedInUserContext = React.createContext();
 
@@ -21,22 +24,52 @@ export const LoggedInUserProvider = ({ children }) => (
 
 export const LoggedInUserPill = props => (
   <LoggedInUserContext.Consumer>
-    {({ user, isLoading }) => (
-      <>
-        <User>
-          <UserIcon
-            width={12}
-            heigh={13}
-            css={`
-              position: relative;
-              top: 2px;
-              margin-right: 5px;
-            `}
-          />
-          {user.email}
-        </User>
-        <Pill last>Logout</Pill>
-      </>
+    {({ user }) => (
+      <Component initialState={{ isOpen: false }}>
+        {({ state: { isOpen }, setState }) => (
+          <Popup
+            trigger={() => (
+              <div
+                css={`
+                  align-items: center;
+                  display: inline-flex;
+                  postion: relative;
+                `}
+              >
+                <Pill onClick={() => setState({ isOpen: !isOpen })}>
+                  <User>
+                    <UserIcon
+                      width={12}
+                      heigh={13}
+                      css={`
+                        position: relative;
+                        top: 2px;
+                        margin-right: 5px;
+                      `}
+                    />
+                    {user.email}
+                  </User>
+                  {CollapsibleArrow({ isOpen })}
+                </Pill>
+              </div>
+            )}
+            position="bottom right"
+            offset={0}
+            open={isOpen}
+            contentStyle={{
+              padding: '0px',
+              border: 'none',
+              borderBottomLeftRadius: '10px',
+              borderBottomRightRadius: '10px',
+              width: 'max-content',
+              minWidth: '172px',
+            }}
+            arrow={false}
+          >
+            <Pill onClick={() => 'logout'}>Logout</Pill>
+          </Popup>
+        )}
+      </Component>
     )}
   </LoggedInUserContext.Consumer>
 );
