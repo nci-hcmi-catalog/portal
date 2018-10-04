851dcb1e9acd89cc1d54fbbacc653578c26af0c1
Alex <lepsalex@gmail.com>
2018-10-04 10:50:47 -0400

    Fix/bugs 18 10 03 (#334)
    * casing on growth rate dimensionality
    
    * Update Footer logos to correct format #126
    
    * Chart Enhancement #320
    
    * Indexing createdAt and UpdatedAt dates on publish
    
    * Multiple values in a line, not list #312
    
    * Arranger - Molecular Characterization/Therpy results in page crash #323

---
+++ ui/src/components/TableList.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Component from 'react-component-component';
+import { List, Toggle } from 'theme/searchStyles';
+
+export default ({
+  value = [],
+  style,
+  liStyle,
+  toggleStyle,
+  limit = 2,
+  expandText = `${value.length - limit} more`,
+  collapseText = 'less',
+}) => (
+  <Component
+    initialState={{
+      expanded: false,
+    }}
+  >
+    {({ state: { expanded }, setState }) =>
+      value.length > 0 ? (
+        <List style={style || {}}>
+          {value.slice(0, expanded ? value.length : limit).map((d, i) => (
+            <div key={i} style={liStyle}>
+              {d}
+            </div>
+          ))}
+          {value.length > limit && (
+            <Toggle
+              style={toggleStyle}
+              onClick={() =>
+                setState(state => ({
+                  expanded: !state.expanded,
+                }))
+              }
+            >
+              {expanded ? `\u25B4 ${collapseText}` : `\u25BE ${expandText}`}
+            </Toggle>
+          )}
+        </List>
+      ) : (
+        'N/A'
+      )
+    }
+  </Component>
+);
