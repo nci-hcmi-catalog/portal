235e26c8afd964c0112a67b7cc0d3230d11c2652
Alex <lepsalex@gmail.com>
2018-10-09 14:28:10 -0400

    Add tooltip to Publish button on Model Page #272

--- ui/src/components/admin/Model/actions/PublishModel.js
+++ ui/src/components/admin/Model/actions/PublishModel.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import Popup from 'reactjs-popup';
+
 import { ModelSingleContext } from '../ModelSingleController';
+
 import { Pill } from 'theme/adminControlsStyles';
 import AdminModelPublishIcon from 'icons/AdminModelPublishIcon';
 
@@ -7,17 +10,43 @@ export default props => (
   <ModelSingleContext.Consumer>
     {({
       state: {
-        form: { isReadyToPublish, values },
+        form: { isReadyToPublish, values, errors },
       },
       publishForm,
     }) => (
-      <Pill
-        disabled={!isReadyToPublish}
-        onClick={() => isReadyToPublish && publishForm(values)}
-        {...props}
+      <Popup
+        trigger={() => (
+          <Pill
+            disabled={!isReadyToPublish}
+            onClick={() => isReadyToPublish && publishForm(values)}
+            {...props}
+          >
+            <AdminModelPublishIcon css={'margin-right: 10px;'} height={16} width={15} />Publish
+          </Pill>
+        )}
+        position="top center"
+        contentStyle={{
+          padding: '6px 12px',
+          border: 'none',
+          borderRadius: '10px',
+          fontFamily: "'Open Sans', sans-serif",
+          fontSize: '13px',
+          width: 'max-content',
+        }}
+        on="hover"
+        offsetY={2}
+        // If button is disabled (not ready to publish) show the tooltip,
+        // like normal, otherwise delay it for a very long time (since there
+        // is no way to disable it all together) and in case someone actually
+        // hovers for 100 seconds then show them a nice message
+        mouseEnterDelay={!isReadyToPublish > 0 ? 100 : 100 * 1000}
       >
-        <AdminModelPublishIcon css={'margin-right: 10px;'} height={16} width={15} />Publish
-      </Pill>
+        {Object.keys(errors).length > 0
+          ? 'Please complete all required fields before publishing'
+          : !isReadyToPublish
+            ? 'No new changes to publish'
+            : 'Ready to Publish =)'}
+      </Popup>
     )}
   </ModelSingleContext.Consumer>
 );
