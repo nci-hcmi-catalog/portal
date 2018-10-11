8207ea0ea66f79b34b11522ba631d8ebc980982c
Rahul Verma <Rahul.Verma@oicr.on.ca>
2018-10-11 16:29:10 -0400

    :lock: Disables Arranger admin endpoints

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,5 @@
 import 'babel-polyfill';
 import express from 'express';
-import socketIO from 'socket.io';
 import { Server } from 'http';
 import Arranger from '@arranger/server';
 import cors from 'cors';
@@ -9,12 +8,9 @@ import lastUpdatedRouter from './lastUpdated';
 const port = process.env.PORT || 5050;
 const app = express();
 const http = Server(app);
-const io = socketIO(http);
 app.use(cors());
 
-Arranger({
-  io,
-}).then(router => {
+Arranger().then(router => {
   app.use(router);
   app.use('/last-updated', lastUpdatedRouter);
 
