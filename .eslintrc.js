ff12c981f0b62a4aca1ddbb61b71995b9cb679e2
Rahul Verma <Rahul.Verma@oicr.on.ca>
2018-10-29 12:47:59 -0400

    :wrench: Enables aria linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
-  extends: ['prettier', 'react-app'],
-  plugins: ['prettier', 'babel'],
+  extends: ['prettier', 'react-app', 'plugin:jsx-a11y/recommended'],
+  plugins: ['prettier', 'babel', 'jsx-a11y'],
   parser: 'babel-eslint',
   rules: {
     'prettier/prettier': [1, { trailingComma: 'all', singleQuote: true }],
