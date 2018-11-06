8b1dda021fa90ceeabe9d4aebac1f3ee616749af
Rahul Verma <Rahul.Verma@oicr.on.ca>
2018-11-06 16:43:46 -0500

    :heavy_minus_sign: Removes unwanted react-axe dependency

--- ui/src/index.js
+++ ui/src/index.js
@@ -7,9 +7,5 @@ import App from './components/App';
 // Set global timezone to UTC
 moment.tz.setDefault('UTC');
 
-if (process.env.NODE_ENV !== 'production') {
-  var axe = require('react-axe');
-  axe(React, ReactDOM, 1000);
-}
 // Render app
 ReactDOM.render(<App />, document.getElementById('root'));
