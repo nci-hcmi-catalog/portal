3cbfd515e223bd8c7d580a74617f40d865036593
Alex <lepsalex@gmail.com>
2018-11-08 15:58:54 -0500

    Update/data wrangle 181108 (#411)
    * New variant migration
    
    * Data wrangled

---
+++ cms/variant-migrations/migrations/20181108152340-insert-variants-181108.js
@@ -0,0 +1,30 @@
+const loadDataFromCSV = require('../lib/loadDataFromCSV');
+
+module.exports = {
+  up(db) {
+    return loadDataFromCSV('variants-18-11-08.csv').then(data =>
+      db.collection('variants').insertMany(data),
+    );
+  },
+  down(db, next) {
+    loadDataFromCSV('variants-18-11-08.csv')
+      .then(data => {
+        return Promise.all(
+          data.map(({ name, type }) => db.collection('variants').findOne({ name, type })),
+        );
+      })
+      .then(docsToRemove => {
+        const idsToRemove = docsToRemove.map(({ _id }) => _id);
+        return db
+          .collection('variants')
+          .deleteMany({ _id: { $in: idsToRemove } })
+          .then(() => {
+            db
+              .collection('models')
+              .updateMany({}, { $pull: { variants: { _id: { $in: idsToRemove } } } });
+          });
+      })
+      .then(() => next())
+      .catch(err => next(err));
+  },
+};
