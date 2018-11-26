58348e952a017fe83db00a472488f35d6494f906
Dušan Andrić <Dusan.Andric@gmail.com>
2018-11-26 13:33:54 -0500

    Use regex for proper svg encoding (#428)

--- ui/src/icons/index.js
+++ ui/src/icons/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 // svg data uris need to be encodeURI()ed plus #'s encoded to work in non webkit
 // https://gist.github.com/clhenrick/6791bb9040a174cd93573f85028e97af
-export const encodeSVG = svg => encodeURI(svg).replace('#', '%23');
+export const encodeSVG = svg => encodeURI(svg).replace(/#/g, '%23');
 
 export const BaseSvg = ({ alt = '', height, width, svg, style, className, ...props }) => (
   <img
