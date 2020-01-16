52bec9cde604a1d011422b27b787a545f67b0027
Minh <hlminh2000@gmail.com>
2020-01-16 16:49:49 -0500

    :fire: removes http prefix in elasticsearch

--- cms/src/services/elastic-search/common/client.js
+++ cms/src/services/elastic-search/common/client.js
@@ -1,5 +1,5 @@
 import { Client } from '@elastic/elasticsearch';
-const eshost = `http://${process.env.ES_HOST || 'localhost'}:${process.env.ES_PORT || 9200}`;
+const eshost = `${process.env.ES_HOST || 'localhost'}:${process.env.ES_PORT || 9200}`;
 const client = new Client({
   node: eshost,
 });
