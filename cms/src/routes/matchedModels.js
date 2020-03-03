a20b49a52dd18f653e3659baf0bb529b5020eddb
Jon Eubank <joneubank@gmail.com>
2020-03-03 17:59:16 -0500

    ✨ Actions to add and remove matches for models

---
+++ cms/src/routes/matchedModels.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import Model from '../schemas/model';
+import MatchedModel from '../schemas/matchedModels';
+
+import MatchUtils from '../helpers/matchedModels';
+
+const actionRouter = express.Router();
+
+actionRouter.post('/connect/:name', async (req, res) => {
+  const { name } = req.params;
+  const { match } = req.body;
+
+  try {
+    const matchedModels = await MatchUtils.connectWithMatchedModels(name, match);
+    res.status(200).json({
+      success: true,
+      message: 'Added to matched models.',
+      matchedModels,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    });
+  }
+});
+
+actionRouter.delete('/:name', async (req, res) => {
+  const { name } = req.params;
+
+  try {
+    const matchedModels = await MatchUtils.removeFromSet(name);
+    res.status(200).json({
+      success: true,
+      message: 'Removed from Matched Models set.',
+      matchedModels,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    });
+  }
+});
+
+export default actionRouter;
