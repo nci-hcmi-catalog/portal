122c1f564efb1a91d62a26a19018c401e4d7ced8
Jon Eubank <joneubank@gmail.com>
2020-04-14 12:24:04 -0400

    ✨ Configure arranger-sets in initializeEs

--- scripts/initializeEs.js
+++ scripts/initializeEs.js
@@ -19,54 +19,109 @@ const date = new Date();
 
 (async () => {
   /** initialize search index */
-  console.log(`Creating search index: ${searchIndex}`);
-  await client.indices.create({
-    index: searchIndex,
-    body: indexSetup,
-  });
-  console.log(`Creating arranger project list: arranger-projects`);
   try {
+    console.log(`\nCreating search index: ${searchIndex}`);
+    await client.indices.create({
+      index: searchIndex,
+      body: indexSetup,
+    });
+    console.log('Success! Created index:', searchIndex);
+  } catch (e) {
+    console.log('Unable to create index:', searchIndex);
+    console.log(e);
+  }
+
+  try {
+    console.log(`\nCreating arranger project list: arranger-projects`);
     await client.indices.create({
       index: 'arranger-projects',
     });
-  } catch (err) {
-    console.log(`failed to create arranger-projects index: ${err}`);
-  }
-  console.log(`Creating arranger project list: ${arrangerProject}`);
-  await client.index({
-    index: 'arranger-projects',
-    body: {
-      id: arrangerProject,
-      active: true,
-      timestamp: date,
-    },
-  });
-  console.log(`Creating arranger project: ${arrangerProject}`);
-  await client.indices.create({
-    index: `arranger-projects-${arrangerProject}`,
-  });
-  await client.index({
-    index: `arranger-projects-${arrangerProject}`,
-    body: {
-      index: searchIndex,
-      name: 'models',
-      timestamp: date,
-      active: true,
-      config: {
-        'aggs-state': {
-          timestamp: date,
-          state: aggsState,
-        },
-        'columns-state': {
-          timestamp: date,
-          state: columnsState,
-        },
-        'matchbox-state': {
-          timestamp: date,
-          state: matchboxState,
-        },
-        extended: extended,
+    console.log(`\nAdding project to arranger project list: ${arrangerProject}`);
+    await client.index({
+      index: 'arranger-projects',
+      body: {
+        id: arrangerProject,
+        active: true,
+        timestamp: date,
       },
-    },
-  });
+    });
+    console.log('Success! Created arranger-projects and added project:', arrangerProject);
+  } catch (e) {
+    console.log('Unable to initialize arranger-projects');
+    console.log(e);
+  }
+
+  const projectIndex = `arranger-projects-${arrangerProject}`;
+  try {
+    console.log(`\nCreating arranger project metadata: ${arrangerProject}`);
+    await client.indices.create({
+      index: projectIndex,
+    });
+    await client.index({
+      index: projectIndex,
+      body: {
+        index: searchIndex,
+        name: 'models',
+        timestamp: date,
+        active: true,
+        config: {
+          'aggs-state': {
+            timestamp: date,
+            state: aggsState,
+          },
+          'columns-state': {
+            timestamp: date,
+            state: columnsState,
+          },
+          'matchbox-state': {
+            timestamp: date,
+            state: matchboxState,
+          },
+          extended: extended,
+        },
+      },
+    });
+    console.log('Success! Created and configured project index:', arrangerProject);
+  } catch (e) {
+    console.log('Unable to set up project meta data:', projectIndex);
+    console.log(e);
+  }
+
+  try {
+    console.log(`\nCreating index: arranger-sets`);
+    await client.indices.create({
+      index: 'arranger-sets',
+      body: {
+        mappings: {
+          properties: {
+            setId: {
+              type: 'keyword',
+            },
+            sqon: {
+              type: 'object',
+            },
+            ids: {
+              type: 'keyword',
+            },
+            type: {
+              type: 'keyword',
+            },
+            path: {
+              type: 'keyword',
+            },
+            size: {
+              type: 'long',
+            },
+            createdAt: {
+              type: 'date',
+            },
+          },
+        },
+      },
+    });
+    console.log('Success! Created arranger-sets');
+  } catch (e) {
+    console.log('Unable to create:', 'arranger-sets');
+    console.log(e);
+  }
 })();
