5a7f526fdf72b9fe06d9bce09d85df625416b464
Rakesh Mistry <mistryrn@mcmaster.ca>
2020-05-11 14:29:12 -0400

    :sparkles: Fetch dictionary data from API (#551)

--- ui/src/components/admin/DataDictionary/DataDictionary.js
+++ ui/src/components/admin/DataDictionary/DataDictionary.js
@@ -6,22 +6,23 @@ import DictionarySidebar from './DictionarySidebar';
 import DictionaryFieldValues from './DictionaryFieldValues';
 import DictionaryDependentFieldValues from './DictionaryDependentFieldValues';
 
+import { getDictionaryDraft } from './../helpers/dictionary';
+
 import { AdminContainer } from 'theme/adminStyles';
 import { AdminDictionaryContent } from 'theme/adminDictionaryStyles';
 import { Row } from 'theme/system';
-
-import dictionaryData from './dictionaryData';
 
 const mainTabWidth = 280;
 
 const DataDictionary = () => {
   const [state, setState] = useContext(DictionaryContext);
 
-  // TODO: replace `dictionaryData` with response from GET request to `/api/v1/dictionary`
   useEffect(() => {
-    setState({
-      ...state,
-      dictionary: dictionaryData,
+    getDictionaryDraft().then(dictionaryData => {
+      setState({
+        ...state,
+        dictionary: dictionaryData,
+      });
     });
   }, []);
 
