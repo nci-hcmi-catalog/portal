e0187f060fde43374543240187d9d3503763216e
Jon Eubank <joneubank@gmail.com>
2020-06-09 14:06:23 -0400

    :sparkles: Healthcheck endpoints for API and CMS

--- cms/src/routes/index.js
+++ cms/src/routes/index.js
@@ -4,3 +4,4 @@ export { default as imagesRouter } from './images';
 export { default as actionRouter } from './action';
 export { default as templatesRouter } from './templates';
 export { default as matchedModelsRouter } from './matchedModels';
+export { default as healthRouter } from './health';
