9d81284cb2dd68c68589a0ff21fb20b9a72d91fc
Rakesh Mistry <mistryrn@mcmaster.ca>
2020-06-15 11:38:20 -0400

    :bug: Change cursor when hovering segments of the pie graphs on the home page (#492)

--- ui/src/components/charts/PrimarySiteChart.js
+++ ui/src/components/charts/PrimarySiteChart.js
@@ -94,7 +94,13 @@ export default ({ sqon, setSQON, victoryRef = React.createRef() }) => (
                   ),
                 )
               }
-            />
+              onMouseEnter={(_data, event) => {
+                event.currentTarget.style.cursor = 'pointer'
+              }}
+              onMouseLeave={(_data, event) => {
+                event.currentTarget.style.cursor = 'auto'
+              }}
+        />
           </>
         );
       }}
