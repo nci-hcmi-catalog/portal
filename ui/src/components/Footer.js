623bbb05227d8b632b90b6f856ead4e957b6f13d
Rakesh Mistry <mistryrn@mcmaster.ca>
2020-07-14 17:46:10 -0400

    ✨Add copyright and help/contact links to the footer * Also add a little bit of responsiveness since the design didn't work too well on smaller screens

--- ui/src/components/Footer.js
+++ ui/src/components/Footer.js
@@ -1,57 +1,88 @@
 import React from 'react';
 
-import base from 'theme';
-import { Row } from 'theme/system';
+import {
+  Footer,
+  FooterNav,
+  FooterNavItem,
+  FooterImg,
+  CopyrightText,
+  CopyrightLink,
+} from 'theme/searchStyles';
+import { Col, Row } from 'theme/system';
 import nihPath from 'assets/logo-NIH-NCI.svg';
 import sangerPath from 'assets/logo-wellcome-sanger-institute.svg';
 import hubPath from 'assets/logo-hub.svg';
 import cancerResearchUKPath from 'assets/logo-cancer-research-UK.svg';
 
-const {
-  keyedPalette: { ironApprox },
-} = base;
-
-const Footer = () => (
-  <footer
-    css={`
-      height: 56px;
-      background-color: #ffffff;
-      border-top: 1px solid ${ironApprox};
-      display: flex;
-      justify-content: center;
-    `}
-  >
-    <Row alignItems="center" justifyContent="space-between" minWidth={147} width="80%">
-      <img
-        src={nihPath}
-        alt="NIH"
-        css={`
-          height: 24px;
-        `}
-      />
-      <img
-        src={sangerPath}
-        alt="Wellcome Sanger Institue"
-        css={`
-          height: 41px;
-        `}
-      />
-      <img
-        src={hubPath}
-        alt="Hubrecht Organoid Technology"
-        css={`
-          height: 41px;
-        `}
-      />
-      <img
-        src={cancerResearchUKPath}
-        alt="Cancer Research UK"
-        css={`
-          height: 41px;
-        `}
-      />
-    </Row>
-  </footer>
+export default () => (
+  <Footer>
+    <Col>
+      <Row alignItems="center">
+        <CopyrightText>
+          &copy; {new Date().getFullYear()} Human Cancer Models Initiative
+        </CopyrightText>
+        <FooterNav>
+          <FooterNavItem>
+            <CopyrightLink
+              href="https://ocg.cancer.gov/programs/HCMI"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              ocg.cancer.gov
+            </CopyrightLink>
+          </FooterNavItem>
+          <FooterNavItem>
+            <CopyrightLink
+              href="https://ocg.cancer.gov/programs/hcmi/hcmi-tutorials"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Help
+            </CopyrightLink>
+          </FooterNavItem>
+          <FooterNavItem>
+            <CopyrightLink
+              href="mailto:ocg@mail.nih.gov"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Contact Us
+            </CopyrightLink>
+          </FooterNavItem>
+        </FooterNav>
+      </Row>
+    </Col>
+    <Col>
+      <Row alignItems="center">
+        <FooterImg
+          src={nihPath}
+          alt="NIH"
+          css={`
+            height: 24px;
+          `}
+        />
+        <FooterImg
+          src={sangerPath}
+          alt="Wellcome Sanger Institue"
+          css={`
+            height: 41px;
+          `}
+        />
+        <FooterImg
+          src={hubPath}
+          alt="Hubrecht Organoid Technology"
+          css={`
+            height: 41px;
+          `}
+        />
+        <FooterImg
+          src={cancerResearchUKPath}
+          alt="Cancer Research UK"
+          css={`
+            height: 41px;
+          `}
+        />
+      </Row>
+    </Col>
+  </Footer>
 );
-
-export default Footer;
