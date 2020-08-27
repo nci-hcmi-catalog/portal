a7507126496895a36aca7a30ed3050fb530dd846
Jon Eubank <joneubank@gmail.com>
2020-08-27 14:52:14 -0400

    Remove new lines between tsv extract rows

--- api/src/dataExport.js
+++ api/src/dataExport.js
@@ -33,7 +33,8 @@ dataExportRouter.post('/:projectId/models', async (req, res) => {
     output
       .pipe(
         map((dataRow, cb) => {
-          cb(null, `${dataRow}${NEW_LINE}`);
+          logger.debug({ dataRow, cb }, 'Arranger Output Row');
+          cb(null, `${dataRow}`);
         }),
       )
       .pipe(res)
