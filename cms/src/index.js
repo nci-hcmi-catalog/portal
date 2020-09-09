8a1dcd8ec9fe1ad228bd9a2c443afc54f0a443c7
Jon Eubank <joneubank@gmail.com>
2020-09-09 12:54:43 -0400

    CMS Healthcheck endpoints are not filtered by authorized user

--- cms/src/index.js
+++ cms/src/index.js
@@ -66,14 +66,8 @@ app.use(bodyParser.json());
 app.use(methodOverride());
 app.use(cors());
 
-// configure logging
-// record user email for each authenticated request
-// app.use(
-//   morgan(
-//     ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent" :req[USER_EMAIL]',
-//   ),
-// );
-app.use(pino({ reqCustomProps: req => ({ user: getLoggedInUser(req).user_email }) }));
+// HealthRouter must be added before the declaration for isUserAuthorized filter
+app.use('/api/v1/health', healthRouter);
 
 if (process.env.AUTH_ENABLED !== 'false') {
   app.use((req, res, next) => {
@@ -86,6 +80,7 @@ if (process.env.AUTH_ENABLED !== 'false') {
     next();
   });
 }
+app.use(pino({ reqCustomProps: req => ({ user: getLoggedInUser(req).user_email }) }));
 
 // configure endpoints
 restify.serve(modelRouter, Model, {
@@ -116,7 +111,7 @@ app.use('/api/v1/bulk', bulkRouter);
 app.use('/api/v1/dictionary', dictionaryRouter);
 app.use('/api/v1/images', imagesRouter);
 app.use('/api/v1/action', actionRouter);
-app.use('/api/v1/health', healthRouter);
+
 app.use('/api/v1/templates', templatesRouter);
 app.use('/api/v1/genomic-variants', variantsRouter);
 app.use('/api/v1/matches', matchedModelsActionsRouter);
