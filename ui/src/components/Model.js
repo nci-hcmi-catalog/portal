32580c2f3ce729f00cd4f8b6e31076a0df5f00c3
Rakesh Mistry <mistryrn@mcmaster.ca>
2021-02-18 15:16:07 -0500

    ✨ Add ATCC Callout to Model Details page for Unexpanded Models (#822)

--- ui/src/components/Model.js
+++ ui/src/components/Model.js
@@ -8,6 +8,7 @@ import ModelBar from 'components/ModelBar';
 import ModelCarouselBar from 'components/ModelCarouselBar';
 import VariantTables from 'components/VariantTables';
 
+import AtccLogo from 'assets/atcc-logo.png';
 import CameraIcon from 'icons/CameraIcon';
 import CheckmarkIcon from 'icons/CheckmarkIcon';
 import ExternalLinkIcon from 'icons/ExternalLinkIcon';
@@ -343,6 +344,29 @@ export default ({ modelName }) => (
                 </Col>
 
                 <Col className="three-col">
+                  {!queryState.model.expanded && (
+                    <div className="model-section__card">
+                      <div>
+                        This is an unexpanded model, which means it has passed sequencing validation
+                        quality control, but is not yet available for purchase.
+                        <img src={AtccLogo} alt="ATCC Logo" />
+                      </div>
+                      <div>
+                        <p>If you would like to have this model prioritized for development:</p>
+                        <ExternalLinkPill
+                          primary
+                          href={
+                            'http://go.atcc.org/HCMIModels?utm_medium=eloqua_landing_page&utm_source=nci&utm_campaign=hcmi_organoid&utm_content=request_additonal_organoids'
+                          }
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <ExternalLinkIcon />
+                          Visit ATCC to Express Interest
+                        </ExternalLinkPill>
+                      </div>
+                    </div>
+                  )}
                   <div className="model-section__card">
                     <h3 className="model-section__card-title">
                       Model Images ({(modelImages && modelImages.length) || '0'})
