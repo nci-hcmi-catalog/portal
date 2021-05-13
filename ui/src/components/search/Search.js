eeb2bfecb41c3f08814f7a44bfb66e292666e01c
Rakesh Mistry <mistryrn@mcmaster.ca>
2021-05-12 22:52:58 -0400

    ♿️ Fix remaining Search page a11y issues (#880) * Add landmark role for Search facets sidebar * Hide nivo charts from screen readers, the library still isn't accessible

--- ui/src/components/search/Search.js
+++ ui/src/components/search/Search.js
@@ -63,7 +63,7 @@ export default ({
           onDragStarted={() => (stable = false)}
           onDragFinished={() => (stable = true)}
         >
-          <Col className="aggregations-wrapper">
+          <Col className="aggregations-wrapper" role="complementary">
             <Component shouldUpdate={() => stable}>
               {() => (
                 <>
@@ -135,6 +135,7 @@ export default ({
                 justify-content: space-around;
                 border: 1px solid #d9d9df;
               `}
+              aria-hidden={true}
             >
               <Component shouldUpdate={() => stable}>
                 {() => (
