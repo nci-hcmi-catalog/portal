4bd9a5c7e245cee124ad35346d004c1cd5d7c200
Jon Eubank <joneubank@gmail.com>
2021-05-18 10:49:16 -0400

    Search table multiple export options

--- ui/src/components/search/Search.js
+++ ui/src/components/search/Search.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Component from 'react-component-component';
 import { Aggregations, CurrentSQON, Table } from '@arranger/components/dist/Arranger';
-import '@arranger/components/public/themeStyles/beagle/beagle.css';
 import SplitPane from 'react-split-pane';
 
 import { SelectedModelsContext } from 'providers/SelectedModels';
@@ -233,7 +232,20 @@ export default ({
                         graphqlField={props.index}
                         InputComponent={TextInput}
                         columnDropdownText="Columns"
-                        exportTSVText="Export All"
+                        enableSelectedTableRowsExporterFilter={true}
+                        selectedRowsFilterPropertyName="_id"
+                        exporterLabel="Export"
+                        exporter={[
+                          {
+                            label: 'TSV (current columns)',
+                            function: 'saveTSV',
+                          },
+                          {
+                            label: 'TSV (all columns)',
+                            function: 'saveTSV',
+                            columns: [],
+                          },
+                        ]}
                         transformParams={params => ({
                           ...params,
                           url: `${globals.ARRANGER_API}/export/${version}/models`,
