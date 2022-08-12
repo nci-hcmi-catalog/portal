078129dcefd3c2abb4d17d419bdfe3ee63e60c9e
Rakesh Mistry <mistryrn@mcmaster.ca>
2022-08-12 11:56:19 -0400

    🎨 Use Hooks for Context * Update `PublishModel` to use the `useContext` hook to access `ModelSingleContext` * Update `SaveModel` to use the `useContext` hook to access `ModelSingleContext`

--- ui/src/components/admin/Model/actions/SaveModel.js
+++ ui/src/components/admin/Model/actions/SaveModel.js
@@ -1,44 +1,41 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { ModelSingleContext } from '../ModelSingleController';
 import { usePublishNotifications } from 'components/admin/Notifications';
 import { ButtonPill } from 'theme/adminControlsStyles';
 import SaveIcon from 'icons/SaveIcon';
 
 const SaveModel = props => {
+  const {
+    state: {
+      form: { values, isReadyToSave },
+      ui: { activeTab },
+    },
+    saveForm,
+  } = useContext(ModelSingleContext);
   const { isPublishingModel } = usePublishNotifications();
 
   return (
-    <ModelSingleContext.Consumer>
-      {({
-        state: {
-          form: { values, isReadyToSave },
-          ui: { activeTab },
-        },
-        saveForm,
-      }) => (
-        <ButtonPill
-          primary
-          disabled={!isReadyToSave || isPublishingModel(values.name)}
-          onClick={async () => {
-            if (isReadyToSave) {
-              switch (activeTab) {
-                case 'edit':
-                  saveForm({ values });
-                  break;
-                case 'images':
-                  break;
-                default:
-                  break;
-              }
-            }
-          }}
-          {...props}
-        >
-          <SaveIcon />
-          Save
-        </ButtonPill>
-      )}
-    </ModelSingleContext.Consumer>
+    <ButtonPill
+      primary
+      disabled={!isReadyToSave || isPublishingModel(values.name)}
+      onClick={async () => {
+        if (isReadyToSave) {
+          switch (activeTab) {
+            case 'edit':
+              saveForm({ values });
+              break;
+            case 'images':
+              break;
+            default:
+              break;
+          }
+        }
+      }}
+      {...props}
+    >
+      <SaveIcon />
+      Save
+    </ButtonPill>
   );
 };
 
