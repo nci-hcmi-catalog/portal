c36bfcc66eacd54f52ada3d18a406706f5c907c8
Rakesh Mistry <mistryrn@mcmaster.ca>
2024-07-09 10:59:32 -0400

    🐛 Fix Bug in and Refactor GDC v2 Sequence URL Migration

--- cms/variant-migrations/migrations/20240708184150-update-models-gdc-v2-sequence-urls.js
+++ cms/variant-migrations/migrations/20240708184150-update-models-gdc-v2-sequence-urls.js
@@ -1,17 +1,19 @@
+const OLD_URL_PATTERN = /https:\/\/portal\.gdc\.cancer\.gov\/v1\/repository\?facetTab=files&filters=%7B%22op%22%3A%22and%22%2C%22content%22%3A%5B%7B%22op%22%3A%22in%22%2C%22content%22%3A%7B%22field%22%3A%22cases\.case_id%22%2C%22value%22%3A%5B%22(.*?)%22%5D%7D%7D%5D%7D&searchTableTab=files/;
+const NEW_URL_PATTERN = /https:\/\/portal\.gdc\.cancer\.gov\/cases\/(.*?)#files/;
+
 module.exports = {
   async up(db) {
     const collection = db.collection('models');
 
     const cursor = collection.find({
       source_sequence_url: {
-        $regex: /https:\/\/portal\.gdc\.cancer\.gov\/v1\/repository\?facetTab=files&filters=%7B%22op%22%3A%22and%22%2C%22content%22%3A%5B%7B%22op%22%3A%22in%22%2C%22content%22%3A%7B%22field%22%3A%22cases\.case_id%22%2C%22value%22%3A%5B%22(.*?)%22%5D%7D%7D%5D%7D&searchTableTab=files/,
+        $regex: OLD_URL_PATTERN,
       },
     });
 
     while (await cursor.hasNext()) {
       const doc = await cursor.next();
-      const pattern = /https:\/\/portal\.gdc\.cancer\.gov\/v1\/repository\?facetTab=files&filters=%7B%22op%22%3A%22and%22%2C%22content%22%3A%5B%7B%22op%22%3A%22in%22%2C%22content%22%3A%7B%22field%22%3A%22cases\.case_id%22%2C%22value%22%3A%5B%22(.*?)%22%5D%7D%7D%5D%7D&searchTableTab=files/;
-      const match = doc.source_sequence_url.match(pattern);
+      const match = doc.source_sequence_url.match(OLD_URL_PATTERN);
 
       if (match) {
         const caseId = match[1];
@@ -26,14 +28,13 @@ module.exports = {
 
     const cursor = collection.find({
       source_sequence_url: {
-        $regex: /https:\/\/portal\.gdc\.cancer\.gov\/cases\/(.*?)#files/,
+        $regex: NEW_URL_PATTERN,
       },
     });
 
     while (await cursor.hasNext()) {
       const doc = await cursor.next();
-      const pattern = /https:\/\/portal\.gdc\.cancer\.gov\/v1\/repository\?facetTab=files&filters=%7B%22op%22%3A%22and%22%2C%22content%22%3A%5B%7B%22op%22%3A%22in%22%2C%22content%22%3A%7B%22field%22%3A%22cases\.case_id%22%2C%22value%22%3A%5B%22(.*?)%22%5D%7D%7D%5D%7D&searchTableTab=files/;
-      const match = doc.source_sequence_url.match(pattern);
+      const match = doc.source_sequence_url.match(NEW_URL_PATTERN);
 
       if (match) {
         const caseId = match[1];
