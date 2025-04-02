a70cb89ab8c0b2d43636f6d00daf2eba89a741cc
Dan DeMaria <ddemaria@oicr.on.ca>
2025-04-02 17:46:14 -0400

    Restore cmsBase config

--- ui/src/components/admin/config.js
+++ ui/src/components/admin/config.js
@@ -1,6 +1,6 @@
 const config = {
   urls: {
-    cmsBase: '',
+    cmsBase: process.env.REACT_APP_CMS_API_URL,
     logoutUrl: process.env.REACT_APP_LOGOUT_URL,
   },
   googleAppId: process.env.REACT_APP_GOOGLE_CLIENT_ID,
