1005663c407615cf55f0c5b19edac7414e3fe97e
Dan DeMaria <ddemaria@oicr.on.ca>
2025-04-17 11:08:33 -0400

    Sets Query

--- ui/src/providers/SavedSets.js
+++ ui/src/providers/SavedSets.js
@@ -1,97 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
+import globals from 'utils/globals';
+import { useDataContext } from '@overture-stack/arranger-components/dist/DataContext';
 
 export const SavedSetsContext = React.createContext();
 
-class SavedSetsProvider extends React.Component {
-  state = {
+const fetchSetsQuery = `query($sqon: JSON) {
+  sets {
+    hits(filters: $sqon) {
+      edges {
+        node {
+          ids
+          setId
+          sqon
+        }
+      }
+    }
+  }
+}`;
+
+const SavedSetsProvider = (props) => {
+  const [state, setState] = useState({
     sets: {},
     loading: false,
-  };
-  render() {
-    return (
-      <SavedSetsContext.Provider
-        value={{
-          state: this.state,
-          createSet: async ({ sqon, sort }) => {
-            this.setState({ loading: true, sets: this.state.sets });
-            const {
-              data: {
-                saveSet: { setId, ids },
-              },
-            } = {};
-            // await api({
-            //   endpoint: `/graphql`,
-            //   body: {
-            //     query: `
-            //     mutation ($sqon: JSON!, $sort: [Sort]) {
-            //       saveSet(sqon: $sqon type: models path:"name" sort:$sort) {
-            //         sqon
-            //         setId
-            //         ids
-            //       }
-            //     }`,
-            //     variables: {
-            //       sqon: sqon || {},
-            //       sort,
-            //     },
-            //   },
-            // });
-            this.setState({
-              loading: false,
-              sets: {
-                ...this.state.sets,
-                [setId]: { sqon, ids },
-              },
-            });
-            return { setId, ids };
-          },
-          fetchSets: async ({ sqon }) => {
-            this.setState({ loading: true, sets: this.state.sets });
-            const { data } = {};
-            // const { data } = await api({
-            //   endpoint: `/graphql`,
-            //   body: {
-            //     query: `query($sqon: JSON) {
-            //     sets {
-            //       hits(filters: $sqon) {
-            //         edges {
-            //           node {
-            //             ids
-            //             setId
-            //             sqon
-            //           }
-            //         }
-            //       }
-            //     }
-            //     }`,
-            //     variables: {
-            //       sqon,
-            //     },
-            //   },
-            // });
-            // const { sets } = data;
-            this.setState({
-              loading: false,
-              sets: {
-                ...this.state.sets,
-                // ...sets.hits.edges.reduce(
-                //   (acc, { node: { setId, ids, sqon } }) => ({ ...acc, [setId]: { ids, sqon } }),
-                //   {},
-                // ),
-              },
-            });
-          },
-          setSet: ({ setId, ids, sqon }) =>
-            this.setState({
-              sets: {
-                ...this.state.sets,
-                [setId]: { ids, sqon },
-              },
-            }),
-        }}
-        {...this.props}
-      />
-    );
-  }
-}
+  });
+
+  const { ARRANGER_API } = globals;
+  const { apiFetcher } = useDataContext({ apiUrl: ARRANGER_API, callerName: 'AggregationQuery' });
+  return (
+    <SavedSetsContext.Provider
+      value={{
+        state,
+        createSet: async ({ sqon, sort }) => {
+          setState({ loading: true, sets: this.state.sets });
+          const {
+            data: {
+              saveSet: { setId, ids },
+            },
+          } = await apiFetcher({ query: fetchSetsQuery, sqon });
+          // await api({
+          //   endpoint: `/graphql`,
+          //   body: {
+          //     query: `
+          //     mutation ($sqon: JSON!, $sort: [Sort]) {
+          //       saveSet(sqon: $sqon type: models path:"name" sort:$sort) {
+          //         sqon
+          //         setId
+          //         ids
+          //       }
+          //     }`,
+          //     variables: {
+          //       sqon: sqon || {},
+          //       sort,
+          //     },
+          //   },
+          // });
+          setState({
+            loading: false,
+            sets: {
+              ...this.state.sets,
+              [setId]: { sqon, ids },
+            },
+          });
+          return { setId, ids };
+        },
+        fetchSets: async ({ sqon }) => {
+          setState({ loading: true, sets: this.state.sets });
+          const { data } = await apiFetcher({ query: fetchSetsQuery, sqon });
+          console.log('sets data', data);
+          const { sets } = data;
+          setState({
+            loading: false,
+            sets: {
+              ...state.sets,
+              // ...sets.hits.edges.reduce(
+              //   (acc, { node: { setId, ids, sqon } }) => ({ ...acc, [setId]: { ids, sqon } }),
+              //   {},
+              // ),
+            },
+          });
+        },
+        setSet: ({ setId, ids, sqon }) =>
+          setState({
+            sets: {
+              ...state.sets,
+              [setId]: { ids, sqon },
+            },
+          }),
+      }}
+      {...props}
+    />
+  );
+};
 export default SavedSetsProvider;
