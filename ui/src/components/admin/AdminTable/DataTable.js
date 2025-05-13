0ba86eba304c111c06910af1eb852cd245c99491
Dan DeMaria <ddemaria@oicr.on.ca>
2025-05-13 10:55:17 -0400

    Remove console.log

--- ui/src/components/admin/AdminTable/DataTable.js
+++ ui/src/components/admin/AdminTable/DataTable.js
@@ -140,7 +140,7 @@ export const GenomicDataTable = ({
 }) => {
   const storedPageSize = pageSizeFromStorage(storageKey);
   const pageSize = storedPageSize ? parseInt(storedPageSize) : state.pageSize || 10;
-  console.log('genomic data table');
+
   return (
     <div css={searchStyles}>
       <ReactTable
