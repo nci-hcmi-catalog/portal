5ecf05964c521b070edc326543e90d86a1a6d7e7
Dan DeMaria <ddemaria@oicr.on.ca>
2025-09-05 14:36:05 -0400

    Update Vite Build

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -5,6 +5,9 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
+    build: {
+      outDir: 'build',
+    },
     define: {
       'process.env': env,
     },
