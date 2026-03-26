a637dc8ae80f5eef46fef31e3cdb1e882e98b4d1
Dan DeMaria <ddemaria@oicr.on.ca>
2026-03-26 11:34:03 -0400

    Update searchClient definition

--- api/src/services/searchClient.ts
+++ api/src/services/searchClient.ts
@@ -1,10 +1,10 @@
 import { buildSearchClient } from '@overture-stack/arranger-server';
 
 const getClient = async () => {
-  const host = process.env.ES_URL || 'http://localhost:9200';
+  const node = process.env.ES_URL || 'http://localhost:9200';
   const user = process.env.ES_USER || '';
   const password = process.env.ES_PASS || '';
-  return await buildSearchClient(host, user, password);
+  return await buildSearchClient({ node, user, password });
 };
 
 export default getClient;
--- cms/src/services/searchClient/client.js
+++ cms/src/services/searchClient/client.js
@@ -1,14 +1,14 @@
 import { buildSearchClient } from '@overture-stack/arranger-server';
 
 const getClient = async () => {
-  const host =
+  const node =
     process.env.ES_HOST && process.env.ES_PORT
       ? `${process.env.ES_HOST}:${process.env.ES_PORT}`
       : 'http://localhost:9200';
   const user = process.env.ES_USER || '';
   const password = process.env.ES_PASS || '';
 
-  return await buildSearchClient(host, user, password);
+  return await buildSearchClient({ node, user, password });
 };
 
 export default getClient;
