477a7852d33994808604c9cf2e6637ecf8561bbb
Dan DeMaria <ddemaria@oicr.on.ca>
2026-05-01 14:11:36 -0400

    Handling for Mongo Collection in republish

--- scripts/utils/republishUtils.js
+++ scripts/utils/republishUtils.js
@@ -8,7 +8,6 @@ import mongoose from 'mongoose';
 
 import { modelStatus } from '../../cms/src/helpers/modelStatus.js';
 import { publishModel } from '../../cms/src/services/search-client/publish.js';
-import Model from '../../cms/src/schemas/model.js';
 import indexLastUpdated from '../../cms/src/services/search-client/indexLastUpdated.js';
 
 import '../../cms/src/schemas/variant.js';
@@ -18,12 +17,18 @@ export const republishModels = async () => {
   console.log('Connecting to MongoDB...');
   // Connect to database
 
-  await mongoose.connect(esUtils.config.MONGODB_URI);
+  const { MONGODB_URI, MONGO_COLLECTION = 'models' } = esUtils.config;
+
+  const client = await mongoose.connect(MONGODB_URI);
   console.log('\nConnected!');
 
-  const models = await Model.find({
-    status: { $in: [modelStatus.published, modelStatus.unpublishedChanges] },
-  });
+  const modelCollection = await client.connection.db.collection(MONGO_COLLECTION);
+
+  const models = await modelCollection
+    .find({
+      status: { $in: [modelStatus.published, modelStatus.unpublishedChanges] },
+    })
+    .toArray();
 
   console.log('\nSearching for models to publish...');
   const names = models.map((i) => i.name);
