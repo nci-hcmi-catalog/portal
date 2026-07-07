1b81694848a20866cc230642c54394d2a81bb9ba
Dan DeMaria <ddemaria@oicr.on.ca>
2026-07-07 17:32:04 -0400

    Update search client helper files

--- api/src/services/searchClient.ts
+++ api/src/services/searchClient.ts
@@ -1,14 +1,32 @@
 import { buildSearchClient } from '@overture-stack/arranger-graphql-router';
+import type { AuthTypes, AuthServices } from '@overture-stack/arranger-types/configs';
 
 import { pm2 } from '../index.ts';
 
 const getClient = async () => {
   const node = process.env.ES_URL || pm2.ES_URL || 'http://localhost:9201';
+  const clientType = process.env.SEARCH_ENGINE || pm2.SEARCH_ENGINE || 'opensearch';
   const username = process.env.ES_USER || pm2.ES_USER || '';
   const password = process.env.ES_PASS || pm2.ES_PASS || '';
-  const client = process.env.SEARCH_CLIENT_TYPE || pm2.SEARCH_CLIENT_TYPE || 'opensearch';
+  const searchEngineAuthType =
+    ((process.env.SEARCH_ENGINE_AUTH_TYPE || pm2.SEARCH_ENGINE_AUTH_TYPE) as AuthTypes) ||
+    undefined;
+  const searchEngineAuthRegion =
+    process.env.SEARCH_ENGINE_AUTH_REGION || pm2.SEARCH_ENGINE_AUTH_REGION || undefined;
+  const searchEngineAuthService =
+    ((process.env.SEARCH_ENGINE_AUTH_SERVICE || pm2.SEARCH_ENGINE_AUTH_SERVICE) as AuthServices) ||
+    undefined;
+  const auth = username
+    ? {
+        username,
+        password,
+        type: searchEngineAuthType,
+        region: searchEngineAuthRegion,
+        service: searchEngineAuthService,
+      }
+    : undefined;
 
-  return await buildSearchClient({ node, username, password, client });
+  return await buildSearchClient({ node, auth, clientType });
 };
 
 export default getClient;
--- cms/src/services/search-client/client.js
+++ cms/src/services/search-client/client.js
@@ -1,17 +1,30 @@
 import { buildSearchClient } from '@overture-stack/arranger-graphql-router';
+import { AuthTypes, AuthServices } from '@overture-stack/arranger-types/configs';
+
+import { pm2 } from '../index.ts';
 
 const getClient = async (pm2config) => {
-  const nodeConfig = pm2config?.node;
-  const node = nodeConfig
-    ? nodeConfig
-    : process.env.ES_HOST && process.env.ES_PORT
-    ? `${process.env.ES_HOST}:${process.env.ES_PORT}`
-    : 'http://localhost:9200';
-  const username = pm2config?.user || process.env.ES_USER || '';
-  const password = pm2config?.password || process.env.ES_PASS || '';
-  const client = pm2config?.clientType || process.env.SEARCH_CLIENT_TYPE || 'opensearch';
+  const node = process.env.ES_URL || pm2config.ES_URL || 'http://localhost:9201';
+  const clientType = process.env.SEARCH_ENGINE || pm2config.SEARCH_ENGINE || 'opensearch';
+  const username = process.env.ES_USER || pm2config.ES_USER || '';
+  const password = process.env.ES_PASS || pm2config.ES_PASS || '';
+  const searchEngineAuthType =
+    process.env.SEARCH_ENGINE_AUTH_TYPE || pm2.SEARCH_ENGINE_AUTH_TYPE || undefined;
+  const searchEngineAuthRegion =
+    process.env.SEARCH_ENGINE_AUTH_REGION || pm2.SEARCH_ENGINE_AUTH_REGION || undefined;
+  const searchEngineAuthService =
+    process.env.SEARCH_ENGINE_AUTH_SERVICE || pm2.SEARCH_ENGINE_AUTH_SERVICE || undefined;
+  const auth = username
+    ? {
+        username,
+        password,
+        type: searchEngineAuthType,
+        region: searchEngineAuthRegion,
+        service: searchEngineAuthService,
+      }
+    : undefined;
 
-  return await buildSearchClient({ node, username, password, client });
+  return await buildSearchClient({ node, auth, clientType });
 };
 
 export default getClient;
