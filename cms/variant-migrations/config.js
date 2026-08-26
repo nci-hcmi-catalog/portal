b528d30bdcdfc975adf001f02129a9bbb3507454
Dan DeMaria <ddemaria@oicr.on.ca>
2026-08-26 10:44:54 -0400

    Working state: migration script

--- cms/variant-migrations/config.js
+++ cms/variant-migrations/config.js
@@ -1,11 +1,15 @@
 'use strict';
+console.log(process.cwd());
 
 module.exports = {
   mongodb: {
     url: process.env.MONGO_URL || 'mongodb://localhost:27017',
     databaseName: process.env.MONGO_DB_NAME || 'hcmi',
   },
-
-  migrationsDir: 'migrations',
+  migrationsDir: './cms/variant-migrations/migrations',
   changelogCollectionName: 'changelog',
+  options: {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
 };
